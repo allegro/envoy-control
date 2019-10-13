55076331ebe31b199f201c8968f80723a70615ed
slonka <slonka@users.noreply.github.com>
2019-10-13 08:24:40 +0200

    Change the way of initializing ProxyContainer

--- envoy-control-tests/src/main/kotlin/pl/allegro/tech/servicemesh/envoycontrol/config/containers/ProxyContainer.kt
+++ envoy-control-tests/src/main/kotlin/pl/allegro/tech/servicemesh/envoycontrol/config/containers/ProxyContainer.kt
@@ -1,6 +1,5 @@
 package pl.allegro.tech.servicemesh.envoycontrol.config.containers
 
-import org.testcontainers.containers.BindMode
 import org.testcontainers.images.builder.ImageFromDockerfile
 import pl.allegro.tech.servicemesh.envoycontrol.testcontainers.GenericContainer
 
@@ -11,16 +10,13 @@ import pl.allegro.tech.servicemesh.envoycontrol.testcontainers.GenericContainer
  * Exapmle: curl -v http://localhost:12345/?call=http://172.24.0.2:9000
  */
 class ProxyContainer : GenericContainer<ProxyContainer>(
-    ImageFromDockerfile()
-        .withDockerfileFromBuilder {
-            it.from("node:10.16.3-jessie")
-                .build()
-        }
-) {
-    override fun configure() {
-        super.configure()
-        withExposedPorts(5678)
-        withClasspathResourceMapping("testcontainers/proxy_server.js", "/index.js", BindMode.READ_ONLY)
-        withCommand("node", "/index.js", "5678")
-    }
-}
+        ImageFromDockerfile()
+                .withFileFromClasspath("index.js", "testcontainers/proxy_server.js")
+                .withDockerfileFromBuilder {
+                    it.from("node:10.16.3-jessie")
+                            .copy("index.js", "index.js")
+                            .expose(5678)
+                            .cmd("node", "index.js", "5678")
+                            .build()
+                }
+)
