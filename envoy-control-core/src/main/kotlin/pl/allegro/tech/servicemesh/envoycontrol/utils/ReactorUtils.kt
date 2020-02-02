8ff3c49e3ceea55141cad9d95b26f478eb56954f
Marcin Falkowski <marcin.falkowski@allegro.pl>
2020-02-02 21:32:24 +0100

    rector metrics: fixed measureBuffer on merge + added measureBuffer on combineLatest

--- envoy-control-core/src/main/kotlin/pl/allegro/tech/servicemesh/envoycontrol/utils/ReactorUtils.kt
+++ envoy-control-core/src/main/kotlin/pl/allegro/tech/servicemesh/envoycontrol/utils/ReactorUtils.kt
@@ -2,8 +2,10 @@ package pl.allegro.tech.servicemesh.envoycontrol.utils
 
 import io.micrometer.core.instrument.MeterRegistry
 import org.slf4j.LoggerFactory
+import reactor.core.Fuseable
 import reactor.core.Scannable
 import reactor.core.publisher.Flux
+import kotlin.streams.asSequence
 
 private val logger = LoggerFactory.getLogger("pl.allegro.tech.servicemesh.envoycontrol.utils.ReactorUtils")
 
@@ -14,31 +16,64 @@ private val logger = LoggerFactory.getLogger("pl.allegro.tech.servicemesh.envoyc
  *   use in a Flux chain AFTER a buffering operator
  * Supported operators:
  * - Flux.create with default BUFFER OverflowStrategy
+ * - Flux.merge (set innerSources parameter to number of sources)
+ * - Flux.combineLatest
  * - onBackpressureBuffer
  * - onBackpressureLatest (buffer size is always 0 or 1)
  * - publishOn
  */
-fun <T> Flux<T>.measureBuffer(name: String, meterRegistry: MeterRegistry): Flux<T> = this.doOnSubscribe {
-    val scannable = Scannable.from(it)
+fun <T> Flux<T>.measureBuffer(
+    name: String, meterRegistry: MeterRegistry, innerSources: Int = 0
+): Flux<T> = this.doOnSubscribe {
 
-    val buffered = scannable.scan(Scannable.Attr.BUFFERED)
-    if (buffered == null) {
-        logger.error("Buffer size not available. Use measureBuffer() only on supported reactor operators")
-        return@doOnSubscribe
+    when (it) {
+        is Fuseable.QueueSubscription<*> -> measureQueueSubscriptionBuffer(it, name, meterRegistry)
+        else -> measureScannableBuffer(Scannable.from(it), name, innerSources, meterRegistry)
     }
+}
 
-    meterRegistry.gauge("reactor-buffers.$name", scannable, { s -> s.scan(Scannable.Attr.BUFFERED)?.toDouble() ?: 0.0 })
+fun <T> Flux<T>.measureDiscardedItems(name: String, meterRegistry: MeterRegistry): Flux<T> = this.doOnDiscard(Any::class.java) {
+    meterRegistry.counter("reactor-discarded-items.$name").increment()
 }
 
 /**
- * Measures how many items were discarded by previous reactor operator. Reports it as a metric.
- *
- * Usage:
- *   use in a Flux chain after a operator, that may discard items.
- *
- * Useful for onBackpressureLatest, onBackpressureDrop
+ * Flux.combineLatest() is an example of QueueSubscription
  */
-fun <T> Flux<T>.measureDiscardedItems(name: String, meterRegistry: MeterRegistry): Flux<T> = this
-    .doOnDiscard(Any::class.java) {
-        meterRegistry.counter("reactor-discarded-items.$name").increment()
+private fun measureQueueSubscriptionBuffer(
+    subscription: Fuseable.QueueSubscription<*>, name: String, meterRegistry: MeterRegistry
+) {
+    meterRegistry.gauge(bufferMetric(name), subscription, queueSubscriptionBufferExtractor)
+}
+
+private fun measureScannableBuffer(scannable: Scannable, name: String, innerSources: Int, meterRegistry: MeterRegistry) {
+    val buffered = scannable.scan(Scannable.Attr.BUFFERED)
+    if (buffered == null) {
+        logger.error("Cannot register metric '${bufferMetric(name)}'. Buffer size not available. " +
+            "Use measureBuffer() only on supported reactor operators")
+        return
     }
+
+    meterRegistry.gauge(bufferMetric(name), scannable, scannableBufferExtractor)
+
+    /**
+     * Special case for FlatMap derived operators like merge(). The main buffer attribute doesn't return actual
+     * buffer (that is controlled by `prefetch` parameter) size. Instead it returns simply number of connected sources.
+     *
+     * To access actual buffer size, we need to extract it from inners(). We don't know how many sources will
+     * be available, so it must be stated explicitly as innerSources parameter.
+     */
+    (0 until innerSources).forEach {
+        meterRegistry.gauge("${bufferMetric(name)}_${it}", scannable, innerBufferExtractor(it))
+    }
+}
+
+private val scannableBufferExtractor = { s: Scannable -> s.scan(Scannable.Attr.BUFFERED)?.toDouble() ?: -1.0 }
+private fun innerBufferExtractor(index: Int) = { s: Scannable ->
+    s.inners().asSequence()
+        .elementAtOrNull(index)
+        ?.let(scannableBufferExtractor)
+        ?: -1.0
+}
+private val queueSubscriptionBufferExtractor = { s: Fuseable.QueueSubscription<*> -> s.size.toDouble() }
+
+private fun bufferMetric(name: String) = "reactor-buffers.$name"
