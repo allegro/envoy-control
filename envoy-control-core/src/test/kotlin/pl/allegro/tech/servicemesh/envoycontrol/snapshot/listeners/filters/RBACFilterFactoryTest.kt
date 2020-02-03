95a5550f952eb16f34a85d27a2b09e4c7f0a36b8
slonka <slonka@users.noreply.github.com>
2020-02-03 14:31:08 +0100

    Use JSON to build RBAC rules in tests

--- envoy-control-core/src/test/kotlin/pl/allegro/tech/servicemesh/envoycontrol/snapshot/listeners/filters/RBACFilterFactoryTest.kt
+++ envoy-control-core/src/test/kotlin/pl/allegro/tech/servicemesh/envoycontrol/snapshot/listeners/filters/RBACFilterFactoryTest.kt
@@ -1,55 +1,45 @@
 package pl.allegro.tech.servicemesh.envoycontrol.snapshot.listeners.filters
 
 import com.google.protobuf.util.JsonFormat
-import io.envoyproxy.envoy.api.v2.route.HeaderMatcher
-import io.envoyproxy.envoy.config.rbac.v2.Permission
-import io.envoyproxy.envoy.config.rbac.v2.Policy
-import io.envoyproxy.envoy.config.rbac.v2.Principal
 import io.envoyproxy.envoy.config.rbac.v2.RBAC
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 import pl.allegro.tech.servicemesh.envoycontrol.groups.Incoming
 import pl.allegro.tech.servicemesh.envoycontrol.groups.IncomingEndpoint
 import pl.allegro.tech.servicemesh.envoycontrol.groups.PathMatchingType
 import pl.allegro.tech.servicemesh.envoycontrol.snapshot.IncomingPermissionsProperties
-import org.assertj.core.api.Assertions.assertThat
 
 internal class RBACFilterFactoryTest {
     val rbacFilterFactory = RBACFilterFactory(IncomingPermissionsProperties())
 
-    val expected = RBAC.newBuilder().putAllPolicies(mapOf(
-            "client1,client2" to Policy.newBuilder().addAllPrincipals(listOf(
-                    Principal.newBuilder().setHeader(
-                            HeaderMatcher.newBuilder().setName("x-service-name").setExactMatch("client1").build()
-                    ).build(),
-                    Principal.newBuilder().setHeader(
-                            HeaderMatcher.newBuilder().setName("x-service-name").setExactMatch("client2").build()
-                    ).build()
-            )).addAllPermissions(listOf(
-                Permission.newBuilder().setAndRules(
-                    Permission.Set.newBuilder().addAllRules(
-                            listOf(
-                                    Permission.newBuilder().setHeader(
-                                            HeaderMatcher.newBuilder()
-                                                    .setName(":path").setExactMatch("/example").build()
-                                    ).build(),
-                                Permission.newBuilder().setOrRules(
-                                        Permission.Set.newBuilder().addAllRules(
-                                                listOf(
-                                                    Permission.newBuilder().setHeader(
-                                                            HeaderMatcher.newBuilder()
-                                                                    .setName(":method").setExactMatch("GET").build()
-                                                    ).build(), Permission.newBuilder().setHeader(
-                                                            HeaderMatcher.newBuilder()
-                                                                    .setName(":method").setExactMatch("POST").build()
-                                                    ).build()
-                                                )
-                                        ).build()
-                                ).build()
-                            )
-                    )
-                ).build()
-            )).build()
-    )).build()
+    private fun headerRule(path: String): String {
+        return """{
+                        "header": {
+                          "name": ":path",
+                          "exact_match": "$path"
+                        }
+                      }"""
+    }
+
+    private fun methodRule(method: String): String {
+        return """{
+              "header": {
+                    "name": ":method",
+                    "exact_match": "$method"
+              }
+        }"""
+    }
+
+    private fun principalHeader(header: String, principal: String): String {
+        val s = """{
+                    "header": {
+                      "name": "$header",
+                      "exact_match": "$principal"
+                    }
+                }"""
+
+        return s
+    }
 
     val exptectedJsonString = """
         {
@@ -59,27 +49,12 @@ internal class RBACFilterFactoryTest {
                 {
                   "and_rules": {
                     "rules": [
-                      {
-                        "header": {
-                          "name": ":path",
-                          "exact_match": "/example"
-                        }
-                      },
+                      ${headerRule("/example")},
                       {
                         "or_rules": {
                           "rules": [
-                            {
-                              "header": {
-                                "name": ":method",
-                                "exact_match": "GET"
-                              }
-                            },
-                            {
-                              "header": {
-                                "name": ":method",
-                                "exact_match": "POST"
-                              }
-                            }
+                            ${methodRule("GET")},
+                            ${methodRule("POST")}
                           ]
                         }
                       }
@@ -87,18 +62,8 @@ internal class RBACFilterFactoryTest {
                   }
                 }
               ], "principals": [
-                {
-                    "header": {
-                      "name": "x-service-name",
-                      "exact_match": "client1"
-                    }
-                },
-                {
-                                    "header": {
-                                      "name": "x-service-name",
-                                      "exact_match": "client2"
-                                    }
-                                }
+                ${principalHeader("x-service-name", "client1")},
+                ${principalHeader("x-service-name", "client2")}
               ]
             }
           }
