cf7674201a913b989104504f3df6894c0caa8009
slonka <slonka@users.noreply.github.com>
2020-02-04 11:41:56 +0100

    Move tests to the top of the file

--- envoy-control-core/src/test/kotlin/pl/allegro/tech/servicemesh/envoycontrol/snapshot/listeners/filters/RBACFilterFactoryTest.kt
+++ envoy-control-core/src/test/kotlin/pl/allegro/tech/servicemesh/envoycontrol/snapshot/listeners/filters/RBACFilterFactoryTest.kt
@@ -15,159 +15,6 @@ import pl.allegro.tech.servicemesh.envoycontrol.groups.ServicesGroup
 import pl.allegro.tech.servicemesh.envoycontrol.snapshot.IncomingPermissionsProperties
 
 internal class RBACFilterFactoryTest {
-    private val rbacFilterFactory = RBACFilterFactory(IncomingPermissionsProperties().also { it.enabled = true })
-
-    private val expectedSimpleEndpointPermissionsJson = """
-        {
-          "policies": {
-            "client1,client2": {
-              "permissions": [
-                {
-                  "and_rules": {
-                    "rules": [
-                      ${headerRule("/example")},
-                      {
-                        "or_rules": {
-                          "rules": [
-                            ${methodRule("GET")},
-                            ${methodRule("POST")}
-                          ]
-                        }
-                      }
-                    ]
-                  }
-                }
-              ], "principals": [
-                ${principalHeader("x-service-name", "client1")},
-                ${principalHeader("x-service-name", "client2")}
-              ]
-            }
-          }
-        }
-    """
-
-    private val expectedTwoClientsSimpleEndpointPermissionsJson = """
-        {
-          "policies": {
-            "client1,client2": {
-              "permissions": [
-                {
-                  "and_rules": {
-                    "rules": [
-                      ${headerRule("/example")},
-                      {
-                        "or_rules": {
-                          "rules": [
-                            ${methodRule("GET")},
-                            ${methodRule("POST")}
-                          ]
-                        }
-                      }
-                    ]
-                  }
-                }, 
-                {
-                  "and_rules": {
-                    "rules": [
-                      ${headerRule("/example2")},
-                      {
-                        "or_rules": {
-                          "rules": [
-                            ${methodRule("GET")},
-                            ${methodRule("POST")}
-                          ]
-                        }
-                      }
-                    ]
-                  }
-                }
-              ], "principals": [
-                ${principalHeader("x-service-name", "client1")},
-                ${principalHeader("x-service-name", "client2")}
-              ]
-            }
-          }
-        }
-    """
-
-    private val exptectedEndpointPermissionsWithoutMethodsJson = """
-        {
-          "policies": {
-            "client1,client2": {
-              "permissions": [
-                {
-                  "and_rules": {
-                    "rules": [
-                      ${headerRule("/example")}
-                    ]
-                  }
-                }
-              ], "principals": [
-                ${principalHeader("x-service-name", "client1")},
-                ${principalHeader("x-service-name", "client2")}
-              ]
-            }
-          }
-        }
-    """
-
-    private val exptectedEndpointPermissionsWithoutClientsJson = """{ "policies": {} }"""
-
-    private fun headerRule(path: String): String {
-        return """{
-            "header": {
-               "name": ":path",
-               "exact_match": "$path"
-            }
-        }"""
-    }
-
-    private fun methodRule(method: String): String {
-        return """{
-           "header": {
-              "name": ":method",
-              "exact_match": "$method"
-           }
-        }"""
-    }
-
-    private fun principalHeader(header: String, principal: String): String {
-        val s = """{
-                    "header": {
-                      "name": "$header",
-                      "exact_match": "$principal"
-                    }
-                }"""
-
-        return s
-    }
-
-    private fun wrapInFilter(json: String): String {
-        return """
-            {
-                "rules": $json
-            }
-        """
-    }
-
-    private fun getRBACFilter(json: String): HttpFilter {
-        val rbacFilter = RBACFilter.newBuilder()
-        JsonFormat.parser().merge(wrapInFilter(json), rbacFilter)
-        return HttpFilter.newBuilder()
-                .setName("envoy.filters.http.rbac")
-                .setTypedConfig(Any.pack(rbacFilter.build()))
-                .build()
-    }
-
-    private fun createGroup(serviceName: String, incomingPermission: Incoming): ServicesGroup {
-        val group = ServicesGroup(
-                ads = false,
-                serviceName = serviceName,
-                proxySettings = ProxySettings(incoming = incomingPermission.copy(permissionsEnabled = true))
-        )
-        return group
-    }
-
     @Test
     fun `should generate RBAC rules for incoming permissions with roles`() {
         // given
@@ -322,4 +169,158 @@ internal class RBACFilterFactoryTest {
         // then
         assertThat(generated).isEqualTo(rbacBuilder)
     }
+
+    private val rbacFilterFactory = RBACFilterFactory(IncomingPermissionsProperties().also { it.enabled = true })
+
+    private val expectedSimpleEndpointPermissionsJson = """
+        {
+          "policies": {
+            "client1,client2": {
+              "permissions": [
+                {
+                  "and_rules": {
+                    "rules": [
+                      ${headerRule("/example")},
+                      {
+                        "or_rules": {
+                          "rules": [
+                            ${methodRule("GET")},
+                            ${methodRule("POST")}
+                          ]
+                        }
+                      }
+                    ]
+                  }
+                }
+              ], "principals": [
+                ${principalHeader("x-service-name", "client1")},
+                ${principalHeader("x-service-name", "client2")}
+              ]
+            }
+          }
+        }
+    """
+
+    private val expectedTwoClientsSimpleEndpointPermissionsJson = """
+        {
+          "policies": {
+            "client1,client2": {
+              "permissions": [
+                {
+                  "and_rules": {
+                    "rules": [
+                      ${headerRule("/example")},
+                      {
+                        "or_rules": {
+                          "rules": [
+                            ${methodRule("GET")},
+                            ${methodRule("POST")}
+                          ]
+                        }
+                      }
+                    ]
+                  }
+                }, 
+                {
+                  "and_rules": {
+                    "rules": [
+                      ${headerRule("/example2")},
+                      {
+                        "or_rules": {
+                          "rules": [
+                            ${methodRule("GET")},
+                            ${methodRule("POST")}
+                          ]
+                        }
+                      }
+                    ]
+                  }
+                }
+              ], "principals": [
+                ${principalHeader("x-service-name", "client1")},
+                ${principalHeader("x-service-name", "client2")}
+              ]
+            }
+          }
+        }
+    """
+
+    private val exptectedEndpointPermissionsWithoutMethodsJson = """
+        {
+          "policies": {
+            "client1,client2": {
+              "permissions": [
+                {
+                  "and_rules": {
+                    "rules": [
+                      ${headerRule("/example")}
+                    ]
+                  }
+                }
+              ], "principals": [
+                ${principalHeader("x-service-name", "client1")},
+                ${principalHeader("x-service-name", "client2")}
+              ]
+            }
+          }
+        }
+    """
+
+    private val exptectedEndpointPermissionsWithoutClientsJson = """{ "policies": {} }"""
+
+    private fun headerRule(path: String): String {
+        return """{
+            "header": {
+               "name": ":path",
+               "exact_match": "$path"
+            }
+        }"""
+    }
+
+    private fun methodRule(method: String): String {
+        return """{
+           "header": {
+              "name": ":method",
+              "exact_match": "$method"
+           }
+        }"""
+    }
+
+    private fun principalHeader(header: String, principal: String): String {
+        val s = """{
+                    "header": {
+                      "name": "$header",
+                      "exact_match": "$principal"
+                    }
+                }"""
+
+        return s
+    }
+
+    private fun wrapInFilter(json: String): String {
+        return """
+            {
+                "rules": $json
+            }
+        """
+    }
+
+    private fun getRBACFilter(json: String): HttpFilter {
+        val rbacFilter = RBACFilter.newBuilder()
+        JsonFormat.parser().merge(wrapInFilter(json), rbacFilter)
+        return HttpFilter.newBuilder()
+                .setName("envoy.filters.http.rbac")
+                .setTypedConfig(Any.pack(rbacFilter.build()))
+                .build()
+    }
+
+    private fun createGroup(serviceName: String, incomingPermission: Incoming): ServicesGroup {
+        val group = ServicesGroup(
+                ads = false,
+                serviceName = serviceName,
+                proxySettings = ProxySettings(incoming = incomingPermission.copy(permissionsEnabled = true))
+        )
+        return group
+    }
+
 }
