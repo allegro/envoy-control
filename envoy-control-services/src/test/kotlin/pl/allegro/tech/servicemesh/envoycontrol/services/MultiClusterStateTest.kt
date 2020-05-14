a60820b6e83636a8f17232ddbad2ea2e88c51b4a
slonka <slonka@users.noreply.github.com>
2020-05-14 14:21:16 +0200

    Review fix

--- envoy-control-services/src/test/kotlin/pl/allegro/tech/servicemesh/envoycontrol/services/MultiClusterStateTest.kt
+++ envoy-control-services/src/test/kotlin/pl/allegro/tech/servicemesh/envoycontrol/services/MultiClusterStateTest.kt
@@ -8,11 +8,14 @@ internal class MultiClusterStateTest {
     @Test
     fun `MultiCLusterStates should implement equality`() {
         // given
-        val element = ServiceInstance("1", address = "0.0.0.0", port = 1, tags = setOf("a"))
-        val serviceInstances = ServiceInstances("a", setOf(element))
-        val l = listOf(ClusterState(ServicesState(mapOf("a" to serviceInstances)), Locality.REMOTE, "dc1"))
-        val multiClusterState1 = MultiClusterState(l)
-        val multiClusterState2 = MultiClusterState(l)
+        val serviceInstance1 = ServiceInstance("1", address = "0.0.0.0", port = 1, tags = setOf("a"))
+        val serviceInstance2 = ServiceInstance("1", address = "0.0.0.0", port = 1, tags = setOf("a"))
+        val serviceInstances1 = ServiceInstances("a", setOf(serviceInstance1))
+        val serviceInstances2 = ServiceInstances("a", setOf(serviceInstance2))
+        val state1 = listOf(ClusterState(ServicesState(mapOf("a" to serviceInstances1)), Locality.REMOTE, "dc1"))
+        val state2 = listOf(ClusterState(ServicesState(mapOf("a" to serviceInstances2)), Locality.REMOTE, "dc1"))
+        val multiClusterState1 = MultiClusterState(state1)
+        val multiClusterState2 = MultiClusterState(state2)
 
         // then
         assertThat(multiClusterState1).isEqualTo(multiClusterState2)
