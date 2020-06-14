123a0f4419dbefdc0538ff583a24d48f984ffcd0
slonka <slonka@users.noreply.github.com>
2020-06-14 11:01:38 +0200

    Start envoy with a delay

--- run-envoy.sh
+++ run-envoy.sh
@@ -1,11 +1,6 @@
 #!/usr/bin/dumb-init /bin/sh
 set -e
 
-sed -i "s/{{.IngressListenerPort}}/${PORT:-10000}/g" /etc/envoy-front-proxy.yaml
-
-/usr/local/bin/envoy --base-id 1 -c /etc/envoy.yaml &
-/usr/local/bin/envoy -c /etc/envoy-front-proxy.yaml &
-
 # start EC
 START_ARGUMENTS=""
 CONFIG_FILE=/etc/envoy-control/application.yaml
@@ -18,6 +13,11 @@ fi
 echo "Launching Envoy-control with $START_ARGUMENTS"
 /bin/envoy-control/envoy-control-runner/bin/envoy-control-runner $START_ARGUMENTS &
 
-sh -c 'sleep 10; curl -X PUT -s localhost:8500/v1/agent/service/register -T /etc/envoy-control/register-echo1.json' &
+# start envoys
+sed -i "s/{{.IngressListenerPort}}/${PORT:-10000}/g" /etc/envoy-front-proxy.yaml
+sh -c 'sleep 10; /usr/local/bin/envoy --base-id 1 -c /etc/envoy.yaml' &
+/usr/local/bin/envoy -c /etc/envoy-front-proxy.yaml &
+
+sh -c 'sleep 12; curl -X PUT -s localhost:8500/v1/agent/service/register -T /etc/envoy-control/register-echo1.json' &
 
 consul agent -server -ui -ui-content-path "/consul/ui" -dev -client 0.0.0.0
