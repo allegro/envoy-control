03b16e838e4e385b3695e7af5175efce15bc0138
Marcin Falkowski <marcin.falkowski@allegro.pl>
2020-06-30 09:54:00 +0200

    change TODO comment about lua script

--- envoy-control-core/src/main/kotlin/pl/allegro/tech/servicemesh/envoycontrol/snapshot/listeners/filters/EnvoyShadowRulesFiltersFactory.kt
+++ envoy-control-core/src/main/kotlin/pl/allegro/tech/servicemesh/envoycontrol/snapshot/listeners/filters/EnvoyShadowRulesFiltersFactory.kt
@@ -6,14 +6,7 @@ import io.envoyproxy.envoy.config.filter.network.http_connection_manager.v2.Http
 
 class EnvoyShadowRulesFiltersFactory {
     companion object {
-        // TODO(mfalkowski): faktyczny skrypt lua nie powinien być przesyłany inline. Inline powinnien być tylko mały
-        //   skrypt startujący, który wczytuje właściwy skrypt. Właściwy skrypt powinien siedzieć razem z envoyem.
-        //   Sprawdz jak mamy w AEC zrobiony skrypt do service tagów:
-        //      alle-envoy-control/src/main/resources/filters/script.lua
-        //      alle-envoy-control/src/test/resources/envoy/extra/lua
-        //      alle-envoy-control/src/main/kotlin/pl/allegro/tech/servicemesh/envoycontrol/alle/intrastructure/filters/AlleEnvoyHttpFiltersFactory.kt
-        //   Do metadanych envoya (a w konsekwencji do ListenersConfig) trzeba wprowadzić nową zmienną: 'enable_lua_ingress_script`, która będzie ustawiana na true
-        //      w nowych wersjach envoy-wrapper, które posiadają już ten skrypt Lua. Tylko wtedy można dodawać ten luaFilter.
+        // TODO(mfalkowski): powinniśmy stworzyć unit testy dla skryptu lua, tak jak mamy we wrapperze
         private val luaScriptContents = this::class.java.classLoader
             .getResource("filters/handler.lua")!!.readText()
 
