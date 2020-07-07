36eaf763bbe79fca20f7121a28340bc0d89738e6
Andrzej Wawrzyniak <andrzej.wawrzyniak@allegro.pl>
2020-07-07 12:37:41 +0200

    remvoed debug messages fro lua test

--- envoy-control-tests/src/main/resources/testcontainers/handler_test.lua
+++ envoy-control-tests/src/main/resources/testcontainers/handler_test.lua
@@ -36,7 +36,6 @@ end
 
 local requestHandleMock = {}
 function requestHandleMock:logInfo(str)
-    print(str)
     table.insert(infos, str)
 end
 
@@ -48,29 +47,21 @@ function requestHandleMock:streamInfo()
     local streamInfo = {}
     function streamInfo:dynamicMetadata()
         local dynamicMetadata = {}
-        print("dynamicMetadata")
         function dynamicMetadata:get(filter)
             if filter ==  "envoy.filters.http.rbac" then
-
-                print("return shadow_engine_result\"]=\"denied")
-
                 return {["shadow_engine_result"]="denied"}
             elseif filter == "envoy.filters.http.lua" then
-
-                print ("GeT dynamicMetadataSet !!!")
                 return dynamicMetadataSet
             end
             fail("Requested invalid filter: " .. filter)
         end
 
         function dynamicMetadata:set(filter, key, value)
-            print("dynamicMetadata:set: '" .. filter  .. "' '" .. key .. "' '" .. value .. "'")
             dynamicMetadataSet[key] = value
         end
 
         return dynamicMetadata
     end
-    print("streamInfo")
     return streamInfo
 end
 
@@ -78,9 +69,6 @@ end
 function requestHandleMock:headers()
     local headersMock = {}
     function headersMock:get(arg)
-
-        print("headersMock:get" .. arg)
-
         if arg == "x-service-name" then
             return serviceName
         elseif arg == ":path" then
@@ -114,13 +102,10 @@ function requestHandleMock:connection()
     local connection = {}
     function connection:ssl()
         if sslConnection == false then
-            print("ssl false return nil")
             return nil
         end
-        print("ssl true return true")
         return true
     end
-    print("connection")
     return connection
 end
 
@@ -140,34 +125,6 @@ function assertNoXCanaryHeaderRequested()
     is(xCanaryHeaderRequested, false)
 end
 
-
-function table_to_string(tbl)
-    local result = "{"
-    for k, v in pairs(tbl) do
-        -- Check the key type (ignore any numerical keys - assume its an array)
-        if type(k) == "string" then
-            result = result.."[\""..k.."\"]".."="
-        end
-
-        -- Check the value type
-        if type(v) == "table" then
-            result = result..table_to_string(v)
-        elseif type(v) == "boolean" then
-            result = result..tostring(v)
-        else
-            result = result.."\""..v.."\""
-        end
-        result = result..","
-    end
-    -- Remove leading commas from the result
-    if result ~= "" then
-        result = result:sub(1, result:len()-1)
-    end
-    return result.."}"
-end
-
-
-
 do -- test: On envoy_on_request should set dynamicMetadata fields
     -- given
     path = "/lorem/ipsum?other=value"
@@ -175,8 +132,6 @@ do -- test: On envoy_on_request should set dynamicMetadata fields
 
     -- when
     handler:envoy_on_request(requestHandleMock)
-
-    --print(table_to_string(dynamicMetadataSet))
 
     -- then
     is_deeply(
@@ -186,8 +141,7 @@ do -- test: On envoy_on_request should set dynamicMetadata fields
           ["request.info.service_name"]="service-first",
           ["request.info.method"]="GET",
           ["request.info.path"]="/lorem/ipsum?other=value"
-      },
-      "invalid dynamicMetadata collection"
+      }
     )
     assertNoWarnings()
     assertNoXCanaryHeaderRequested()
@@ -205,8 +159,6 @@ do -- test: On envoy_on_response should show Access denied log
     handler:envoy_on_request(requestHandleMock)
     handler:envoy_on_response(requestHandleMock)
 
-    print(table_to_string(infos))
-
     -- then
     is_deeply(
         infos,
@@ -215,8 +167,6 @@ do -- test: On envoy_on_response should show Access denied log
             "running envoy_on_response",
             "\nAccess denied for request: method = GET, path = /lorem/ipsum?other=value, clientIp = Forwarded: for=192.0.2.60;proto=http;by=203.0.113.43, clientName = service-first, protocol = https, statusCode = 200"
         }
-        ,
-        "invalid log infos"
     )
     assertNoWarnings()
     assertNoXCanaryHeaderRequested()
