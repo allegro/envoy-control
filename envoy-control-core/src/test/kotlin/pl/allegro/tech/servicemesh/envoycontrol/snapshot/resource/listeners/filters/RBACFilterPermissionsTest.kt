418af6cc90a05f433ac39de54f376c1f8d26a68a
wookieJ <lukaszjedryczka.biuro@gmail.com>
2020-08-31 10:44:10 +0200

    Safe regex paths

--- envoy-control-core/src/test/kotlin/pl/allegro/tech/servicemesh/envoycontrol/snapshot/resource/listeners/filters/RBACFilterPermissionsTest.kt
+++ envoy-control-core/src/test/kotlin/pl/allegro/tech/servicemesh/envoycontrol/snapshot/resource/listeners/filters/RBACFilterPermissionsTest.kt
@@ -72,6 +72,40 @@ internal class RBACFilterPermissionsTest {
     }
 
     @Test
+    fun `should create permissions with exact path matcher with params in path`() {
+        // given
+        val endpoint = IncomingEndpoint(
+            path = "/example/{param1}/rest/{param2}/end",
+            pathMatchingType = PathMatchingType.PATH,
+            methods = setOf()
+        )
+        // language=json
+        val expectedPermission = """{
+            "and_rules": {
+              "rules": [
+                {
+                  "url_path": {
+                    "path": {
+                      "safe_regex": {
+                        "google_re2": {
+                        },
+                        "regex": "\\/example\\/\\w+\\/rest\\/\\w+\\/end"
+                      }
+                    }
+                  }
+                }
+              ]
+            }
+        }""".asPermission()
+
+        // when
+        val permissions = rbacFilterPermissions.createCombinedPermissions(endpoint).build()
+
+        // then
+        assertThat(permissions).isEqualTo(expectedPermission)
+    }
+
+    @Test
     fun `should create permissions with prefix path matcher with params in path`() {
         // given
         val endpoint = IncomingEndpoint(
@@ -86,7 +120,11 @@ internal class RBACFilterPermissionsTest {
                 {
                   "url_path": {
                     "path": {
-                      "prefix": "/example/\\w+/rest/\\w+/end"
+                      "safe_regex": {
+                        "google_re2": {
+                        },
+                        "regex": "\\/example\\/\\w+\\/rest\\/\\w+\\/end*"
+                      }
                     }
                   }
                 }
