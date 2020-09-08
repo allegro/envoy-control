cc718198312687cd57dd5be7a8393b9851055ce2
wookieJ <lukaszjedryczka.biuro@gmail.com>
2020-09-08 11:38:49 +0200

    Path regex type added

--- envoy-control-tests/src/main/kotlin/pl/allegro/tech/servicemesh/envoycontrol/permissions/IncomingPermissionsEndpointsTest.kt
+++ envoy-control-tests/src/main/kotlin/pl/allegro/tech/servicemesh/envoycontrol/permissions/IncomingPermissionsEndpointsTest.kt
@@ -1,44 +1,41 @@
 package pl.allegro.tech.servicemesh.envoycontrol.permissions
 
-import okhttp3.Headers
 import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.BeforeAll
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
-import org.junit.jupiter.api.extension.RegisterExtension
 import org.testcontainers.junit.jupiter.Container
+import pl.allegro.tech.servicemesh.envoycontrol.assertions.hasNoRBACDenials
+import pl.allegro.tech.servicemesh.envoycontrol.assertions.hasOneAccessDenialWithActionBlock
 import pl.allegro.tech.servicemesh.envoycontrol.assertions.isRbacAccessLog
 import pl.allegro.tech.servicemesh.envoycontrol.config.Echo1EnvoyAuthConfig
+import pl.allegro.tech.servicemesh.envoycontrol.config.Echo2EnvoyAuthConfig
 import pl.allegro.tech.servicemesh.envoycontrol.config.EnvoyControlTestConfiguration
-import pl.allegro.tech.servicemesh.envoycontrol.config.consul.ConsulExtension
 import pl.allegro.tech.servicemesh.envoycontrol.config.containers.ToxiproxyContainer
 import pl.allegro.tech.servicemesh.envoycontrol.config.envoy.EnvoyContainer
-import pl.allegro.tech.servicemesh.envoycontrol.config.envoy.EnvoyExtension
-import pl.allegro.tech.servicemesh.envoycontrol.config.envoycontrol.EnvoyControlExtension
 import pl.allegro.tech.servicemesh.envoycontrol.config.envoycontrol.EnvoyControlRunnerTestApp
 
-@SuppressWarnings("LargeClass")
-internal class IncomingPermissionsEndpointsTest : EnvoyControlTestConfiguration() {
+class IncomingPermissionsEndpointsTest : EnvoyControlTestConfiguration() {
 
     companion object {
-
-        @JvmField
-        @RegisterExtension
-        val consul = ConsulExtension()
-
-        @JvmField
-        @RegisterExtension
-        val envoyControl = EnvoyControlExtension(consul, mapOf(
-            "envoy-control.envoy.snapshot.incoming-permissions.enabled" to true,
-            "envoy-control.envoy.snapshot.routes.status.create-virtual-cluster" to true
-        ))
+        private const val prefix = "envoy-control.envoy.snapshot"
+        private val properties = { sourceClientIp: String ->
+            mapOf(
+                "$prefix.incoming-permissions.enabled" to true,
+                "$prefix.incoming-permissions.source-ip-authentication.ip-from-range.source-ip-client" to
+                    "$sourceClientIp/32",
+                "$prefix.routes.status.create-virtual-cluster" to true
+            )
+        }
 
         // language=yaml
-        private val echoOneYaml = """
-            node: 
-              metadata: 
-                proxy_settings: 
+        private fun proxySettings(unlistedEndpointsPolicy: String) = """
+            node:
+              metadata:
+                proxy_settings:
                   incoming:
-                    unlistedEndpointsPolicy: blockAndLog
+                    unlistedEndpointsPolicy: $unlistedEndpointsPolicy
                     endpoints:
                     - path: "/path"
                       clients: ["echo2"]
@@ -49,40 +46,22 @@ internal class IncomingPermissionsEndpointsTest : EnvoyControlTestConfiguration(
                     roles: []
                   outgoing:
                     dependencies:
+                      - service: "echo"
                       - service: "echo2"
         """.trimIndent()
 
-        // language=yaml
-        private val echoTwoYaml = """
-            node:
-              metadata:
-                proxy_settings:
-                  outgoing:
-                    dependencies:
-                      - service: "echo1"
-        """.trimIndent()
+        private val echoConfig = Echo1EnvoyAuthConfig.copy(
+            configOverride = proxySettings(unlistedEndpointsPolicy = "blockAndLog"))
 
-        private val echoOneConfig = Echo1EnvoyAuthConfig.copy(configOverride = echoOneYaml)
-        private val echoTwoConfig = Echo1EnvoyAuthConfig.copy(configOverride = echoTwoYaml)
+        private val echo2Config = Echo2EnvoyAuthConfig.copy(
+            configOverride = proxySettings(unlistedEndpointsPolicy = "blockAndLog"))
 
-        @JvmField
-        @RegisterExtension
-        val envoyOne = EnvoyExtension(envoyControl, config = echoOneConfig)
-
-        @JvmField
-        @RegisterExtension
-        val envoyTwo = EnvoyExtension(envoyControl, config = echoTwoConfig)
-
-        private val echo1Envoy by lazy { envoyContainer1 }
+        private val echoEnvoy by lazy { envoyContainer1 }
         private val echo2Envoy by lazy { envoyContainer2 }
 
         @Container
         private val sourceIpClient = ToxiproxyContainer(exposedPortsCount = 2).withNetwork(network)
-        private val sourceIpClientToEchoProxy by lazy { sourceIpClient.createProxyToEnvoyIngress(envoy = echo1Envoy) }
-        private val sourceIpClientToEcho2Proxy by lazy { sourceIpClient.createProxyToEnvoyIngress(envoy = echo2Envoy) }
-
         private val echoLocalService by lazy { localServiceContainer }
-        private val echo2LocalService by lazy { echoContainer2 }
 
         @JvmStatic
         @BeforeAll
@@ -91,11 +70,9 @@ internal class IncomingPermissionsEndpointsTest : EnvoyControlTestConfiguration(
                 EnvoyControlRunnerTestApp(properties = properties(sourceIpClient.ipAddress()), consulPort = consulPort)
             },
                 envoys = 2,
-                envoyConfig = echoOneConfig,
-                secondEnvoyConfig = echoTwoConfig
+                envoyConfig = echoConfig,
+                secondEnvoyConfig = echo2Config
             )
-            echo3Envoy = createEnvoyContainerWithEcho3Certificate(configOverride = IncomingPermissionsLoggingModeTest.echo3Config)
-            echo3Envoy.start()
 
             registerServiceWithEnvoyOnIngress(name = "echo", envoy = echoEnvoy, tags = listOf("mtls:enabled"))
             registerServiceWithEnvoyOnIngress(name = "echo2", envoy = echo2Envoy, tags = listOf("mtls:enabled"))
@@ -105,8 +82,6 @@ internal class IncomingPermissionsEndpointsTest : EnvoyControlTestConfiguration(
 
         private fun waitForEnvoysInitialized() {
             untilAsserted {
-                assertThat(echo3Envoy.admin().isEndpointHealthy("echo", echoEnvoy.ipAddress())).isTrue()
-                assertThat(echo3Envoy.admin().isEndpointHealthy("echo2", echo2Envoy.ipAddress())).isTrue()
                 assertThat(echoEnvoy.admin().isEndpointHealthy("echo2", echo2Envoy.ipAddress())).isTrue()
                 assertThat(echo2Envoy.admin().isEndpointHealthy("echo", echoEnvoy.ipAddress())).isTrue()
             }
@@ -114,56 +89,91 @@ internal class IncomingPermissionsEndpointsTest : EnvoyControlTestConfiguration(
     }
 
     @Test
-    fun `echo should allow echo3 to access 'example-endpoint-action1' on exact path with param`() {
+    fun `echo should allow echo2 to access 'path' endpoint`() {
         // when
-        val echoResponeOne = envoyOne.ingressOperations.callLocalService(endpoint = "/path", headers = Headers.of())
-//        val echoResponseOne = call(service = "echo", from = envoyTwo, path = "/example-endpoint/1/action1")
-//        val echoResponseTwo = call(service = "echo", from = envoyTwo, path = "/example-endpoint/2/action1")
-//        val echoResponseThree = call(service = "echo", from = envoyTwo, path = "/example-endpoint/3/action1")
-//
-//        // then
-        assertThat(echoResponeOne).isOk()
-//        assertThat(echoResponseTwo).isOk().isFrom(echoLocalService)
-//        assertThat(echoResponseThree).isOk().isFrom(echoLocalService)
-//        assertThat(echoEnvoy.ingressSslRequests).isEqualTo(3)
-//        assertThat(echoEnvoy).hasNoRBACDenials()
+        val echoResponse = call(service = "echo", from = echo2Envoy, path = "/path")
+
+        // then
+        assertThat(echoResponse).isOk().isFrom(echoLocalService)
+        assertThat(echoEnvoy.ingressSslRequests).isOne()
+        assertThat(echoEnvoy).hasNoRBACDenials()
     }
 
     @Test
-    fun `echo should NOT allow echo3 to access 'wildcard-in-the-middle' on path with param and with wildcard`() {
-//        // when
-//        val echoResponseAllowed = call(service = "echo", from = echo3Envoy, path = "/wildcard-in-the-middle/1/rest")
-//        val echoResponseNotAllowed = call(service = "echo", from = echo3Envoy, path = "/wildcard-in-the-middles/1/rest")
-//
-//        // then
-//        assertThat(echoResponseAllowed).isOk().isFrom(echoLocalService)
-//        assertThat(echoResponseNotAllowed).isForbidden()
-//        assertThat(echoEnvoy.ingressSslRequests).isEqualTo(2)
-//        assertThat(echoEnvoy).hasOneAccessDenialWithActionBlock(
-//            protocol = "https",
-//            path = "/wildcard-in-the-middles/1/rest",
-//            method = "GET",
-//            clientName = "echo3",
-//            clientIp = echo3Envoy.ipAddress()
-//        )
+    fun `echo should NOT allow echo2 to access 'path' endpoint with following segments`() {
+        // when
+        val echoResponse = call(service = "echo", from = echo2Envoy, path = "/path/sub-segment")
+
+        // then
+        assertThat(echoResponse).isForbidden()
+        assertThat(echoEnvoy.ingressSslRequests).isOne()
+        assertThat(echoEnvoy).hasOneAccessDenialWithActionBlock(
+            protocol = "https",
+            path = "/path/sub-segment",
+            method = "GET",
+            clientName = "echo2",
+            clientIp = echo2Envoy.ipAddress()
+        )
     }
 
-//    @BeforeEach
-//    fun startRecordingRBACLogs() {
-//        echoEnvoy.recordRBACLogs()
-//        echo2Envoy.recordRBACLogs()
-//    }
-//
-//    fun stopRecordingRBACLogs() {
-//        echoEnvoy.logRecorder.stopRecording()
-//        echo2Envoy.logRecorder.stopRecording()
-//    }
-//
-//    @AfterEach
-//    override fun cleanupTest() {
-//        listOf(echoEnvoy, echo2Envoy, echo3Envoy).forEach { it.admin().resetCounters() }
-//        stopRecordingRBACLogs()
-//    }
+    @Test
+    fun `echo should allow echo2 to access 'prefix' endpoint`() {
+        // when
+        val echoResponseOne = call(service = "echo", from = echo2Envoy, path = "/prefix")
+        val echoResponseTwo = call(service = "echo", from = echo2Envoy, path = "/prefixes")
+        val echoResponseThree = call(service = "echo", from = echo2Envoy, path = "/prefix/segment")
+
+        // then
+        assertThat(echoResponseOne).isOk().isFrom(echoLocalService)
+        assertThat(echoResponseTwo).isOk().isFrom(echoLocalService)
+        assertThat(echoResponseThree).isOk().isFrom(echoLocalService)
+        assertThat(echoEnvoy.ingressSslRequests).isEqualTo(3)
+        assertThat(echoEnvoy).hasNoRBACDenials()
+    }
+
+    @Test
+    fun `echo should allow echo2 to access 'regex' endpoint`() {
+        // when
+        val echoResponseOne = call(service = "echo", from = echo2Envoy, path = "/regex/1/segment")
+        val echoResponseTwo = call(service = "echo", from = echo2Envoy, path = "/regex/param-1/segment")
+
+        // then
+        assertThat(echoResponseOne).isOk().isFrom(echoLocalService)
+        assertThat(echoResponseTwo).isOk().isFrom(echoLocalService)
+        assertThat(echoEnvoy.ingressSslRequests).isEqualTo(2)
+        assertThat(echoEnvoy).hasNoRBACDenials()
+    }
+
+    @Test
+    fun `echo should allow echo2 to access 'regex' endpoint without valid segments`() {
+        // when
+        val echoResponseOne = call(service = "echo", from = echo2Envoy, path = "/regex/1")
+        val echoResponseTwo = call(service = "echo", from = echo2Envoy, path = "/regex/param/seg")
+        val echoResponseThree = call(service = "echo", from = echo2Envoy, path = "/regex/param/segment/last-segment")
+
+        // then
+        assertThat(echoResponseOne).isForbidden()
+        assertThat(echoResponseTwo).isForbidden()
+        assertThat(echoResponseThree).isForbidden()
+        assertThat(echoEnvoy.ingressSslRequests).isEqualTo(3)
+    }
+
+    @BeforeEach
+    fun startRecordingRBACLogs() {
+        echoEnvoy.recordRBACLogs()
+        echo2Envoy.recordRBACLogs()
+    }
+
+    fun stopRecordingRBACLogs() {
+        echoEnvoy.logRecorder.stopRecording()
+        echo2Envoy.logRecorder.stopRecording()
+    }
+
+    @AfterEach
+    override fun cleanupTest() {
+        listOf(echoEnvoy, echo2Envoy).forEach { it.admin().resetCounters() }
+        stopRecordingRBACLogs()
+    }
 
     private val EnvoyContainer.ingressSslRequests: Int?
         get() = this.admin().statValue("http.ingress_https.downstream_rq_completed")?.toInt()
