569112a52e14850cb8c4650865733afaa4690f37
Krzysztof Słonka <slonka@users.noreply.github.com>
2020-09-25 12:09:38 +0200

    Revert "Update to v3 step 1 (#173)"
    This reverts commit 649f921d7dc032c2dd8ec0c100c351b9c8d1eb2a.

--- envoy-control-core/src/test/java/pl/allegro/tech/servicemesh/envoycontrol/SimpleCacheWithMissingEndpointsTest.java
+++ envoy-control-core/src/test/java/pl/allegro/tech/servicemesh/envoycontrol/SimpleCacheWithMissingEndpointsTest.java
@@ -1,10 +1,9 @@
-package pl.allegro.tech.servicemesh.envoycontrol.v2;
+package pl.allegro.tech.servicemesh.envoycontrol;
 
 import com.google.common.collect.ImmutableList;
 import io.envoyproxy.controlplane.cache.Resources;
+import io.envoyproxy.controlplane.cache.Snapshot;
 import io.envoyproxy.controlplane.cache.Watch;
-import io.envoyproxy.controlplane.cache.XdsRequest;
-import io.envoyproxy.controlplane.cache.v2.Snapshot;
 import io.envoyproxy.envoy.api.v2.ClusterLoadAssignment;
 import io.envoyproxy.envoy.api.v2.DiscoveryRequest;
 import io.envoyproxy.envoy.api.v2.core.Node;
@@ -15,7 +14,7 @@ import java.util.Collections;
 
 import static java.util.Collections.emptyList;
 
-public class SimpleCacheWithMissingEndpointsTest extends pl.allegro.tech.servicemesh.envoycontrol.v2.SimpleCacheTest {
+public class SimpleCacheWithMissingEndpointsTest extends SimpleCacheTest {
 
     @Override
     protected boolean shouldSendMissingEndpoints() {
@@ -41,7 +40,7 @@ public class SimpleCacheWithMissingEndpointsTest extends pl.allegro.tech.service
 
     @Test
     public void missingNamesListShouldReturnWatcherWithResponseInAdsMode() {
-        pl.allegro.tech.servicemesh.envoycontrol.v2.SimpleCache<String> cache = new pl.allegro.tech.servicemesh.envoycontrol.v2.SimpleCache<>(new SingleNodeGroup(), shouldSendMissingEndpoints());
+        SimpleCache<String> cache = new SimpleCache<>(new SingleNodeGroup(), shouldSendMissingEndpoints());
 
         cache.setSnapshot(SingleNodeGroup.GROUP, MULTIPLE_RESOURCES_SNAPSHOT2);
 
@@ -49,12 +48,12 @@ public class SimpleCacheWithMissingEndpointsTest extends pl.allegro.tech.service
 
         Watch watch = cache.createWatch(
                 true,
-                XdsRequest.create(DiscoveryRequest.newBuilder()
+                DiscoveryRequest.newBuilder()
                         .setNode(Node.getDefaultInstance())
-                        .setTypeUrl(Resources.V2.ENDPOINT_TYPE_URL)
+                        .setTypeUrl(Resources.ENDPOINT_TYPE_URL)
                         .addResourceNames("none")
                         .addResourceNames(CLUSTER_NAME)
-                        .build()),
+                        .build(),
                 Collections.emptySet(),
                 responseTracker,
                 false);
