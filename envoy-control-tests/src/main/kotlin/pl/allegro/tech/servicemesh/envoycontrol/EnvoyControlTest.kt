79ca64d7e9d09cccc2fce6bbc918a2a8252528ed
Piotr Betkier <pbetkier@gmail.com>
2020-10-29 13:08:36 +0100

    Another batch of revised tests (#203)
    * revised HttpIdleTimeoutTest
    
    * revised HealthIndicatorTest
    
    * revised EnvoyControlTest

--- envoy-control-tests/src/main/kotlin/pl/allegro/tech/servicemesh/envoycontrol/EnvoyControlTest.kt
+++ envoy-control-tests/src/main/kotlin/pl/allegro/tech/servicemesh/envoycontrol/EnvoyControlTest.kt
@@ -1,90 +1,199 @@
 package pl.allegro.tech.servicemesh.envoycontrol
 
 import org.assertj.core.api.Assertions.assertThat
-import org.junit.jupiter.api.BeforeAll
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.RegisterExtension
+import pl.allegro.tech.servicemesh.envoycontrol.assertions.isFrom
+import pl.allegro.tech.servicemesh.envoycontrol.assertions.isOk
+import pl.allegro.tech.servicemesh.envoycontrol.assertions.untilAsserted
 import pl.allegro.tech.servicemesh.envoycontrol.config.Ads
 import pl.allegro.tech.servicemesh.envoycontrol.config.AdsWithStaticListeners
-import pl.allegro.tech.servicemesh.envoycontrol.config.EnvoyControlTestConfiguration
 import pl.allegro.tech.servicemesh.envoycontrol.config.Xds
+import pl.allegro.tech.servicemesh.envoycontrol.config.consul.ConsulExtension
+import pl.allegro.tech.servicemesh.envoycontrol.config.envoy.EnvoyExtension
+import pl.allegro.tech.servicemesh.envoycontrol.config.envoycontrol.EnvoyControlExtension
+import pl.allegro.tech.servicemesh.envoycontrol.config.service.EchoServiceExtension
 
-internal class AdsWithStaticListenersEnvoyControlTest : EnvoyControlTest() {
+class AdsWithStaticListenersEnvoyControlTest : EnvoyControlTest {
     companion object {
 
-        @JvmStatic
-        @BeforeAll
-        fun adsSetup() {
-            setup(envoyConfig = AdsWithStaticListeners)
-        }
+        @JvmField
+        @RegisterExtension
+        val consul = ConsulExtension()
+
+        @JvmField
+        @RegisterExtension
+        val envoyControl = EnvoyControlExtension(consul)
+
+        @JvmField
+        @RegisterExtension
+        val service = EchoServiceExtension()
+
+        @JvmField
+        @RegisterExtension
+        val redeployedService = EchoServiceExtension()
+
+        @JvmField
+        @RegisterExtension
+        val envoy = EnvoyExtension(envoyControl, config = AdsWithStaticListeners)
     }
+
+    override fun consul() = consul
+
+    override fun envoyControl() = envoyControl
+
+    override fun service() = service
+
+    override fun redeployedService() = redeployedService
+
+    override fun envoy() = envoy
 }
 
-internal class AdsEnvoyControlTest : EnvoyControlTest() {
+class AdsEnvoyControlTest : EnvoyControlTest {
     companion object {
 
-        @JvmStatic
-        @BeforeAll
-        fun adsSetup() {
-            setup(envoyConfig = Ads)
-        }
+        @JvmField
+        @RegisterExtension
+        val consul = ConsulExtension()
+
+        @JvmField
+        @RegisterExtension
+        val envoyControl = EnvoyControlExtension(consul)
+
+        @JvmField
+        @RegisterExtension
+        val service = EchoServiceExtension()
+
+        @JvmField
+        @RegisterExtension
+        val redeployedService = EchoServiceExtension()
+
+        @JvmField
+        @RegisterExtension
+        val envoy = EnvoyExtension(envoyControl, config = Ads)
     }
+
+    override fun consul() = consul
+
+    override fun envoyControl() = envoyControl
+
+    override fun service() = service
+
+    override fun redeployedService() = redeployedService
+
+    override fun envoy() = envoy
 }
 
-internal class XdsEnvoyControlTest : EnvoyControlTest() {
+class XdsEnvoyControlTest : EnvoyControlTest {
     companion object {
 
-        @JvmStatic
-        @BeforeAll
-        fun nonAdsSetup() {
-            setup(envoyConfig = Xds)
-        }
+        @JvmField
+        @RegisterExtension
+        val consul = ConsulExtension()
+
+        @JvmField
+        @RegisterExtension
+        val envoyControl = EnvoyControlExtension(consul)
+
+        @JvmField
+        @RegisterExtension
+        val service = EchoServiceExtension()
+
+        @JvmField
+        @RegisterExtension
+        val redeployedService = EchoServiceExtension()
+
+        @JvmField
+        @RegisterExtension
+        val envoy = EnvoyExtension(envoyControl, config = Xds)
     }
+
+    override fun consul() = consul
+
+    override fun envoyControl() = envoyControl
+
+    override fun service() = service
+
+    override fun redeployedService() = redeployedService
+
+    override fun envoy() = envoy
 }
 
-internal abstract class EnvoyControlTest : EnvoyControlTestConfiguration() {
+interface EnvoyControlTest {
+
+    fun consul(): ConsulExtension
+
+    fun envoyControl(): EnvoyControlExtension
+
+    fun service(): EchoServiceExtension
+
+    fun redeployedService(): EchoServiceExtension
+
+    fun envoy(): EnvoyExtension
 
     @Test
     fun `should allow proxy-ing request using envoy`() {
         // given
-        registerService(name = "echo")
+        consul().server.operations.registerService(service(), name = "echo")
 
         untilAsserted {
             // when
-            val response = callEcho()
+            val response = envoy().egressOperations.callService("echo")
 
             // then
-            assertThat(response).isOk().isFrom(echoContainer)
+            assertThat(response).isOk().isFrom(service())
         }
     }
 
     @Test
     fun `should route traffic to the second instance when first is deregistered`() {
         // given
-        val id = registerService(name = "echo")
+        val id = consul().server.operations.registerService(service(), name = "echo")
 
         untilAsserted {
-            // when
-            val response = callEcho()
-
-            // then
-            assertThat(response).isOk().isFrom(echoContainer)
+            val response = envoy().egressOperations.callService("echo")
+            assertThat(response).isOk().isFrom(service())
         }
 
-        checkTrafficRoutedToSecondInstance(id)
+        // when
+        redeployEchoService(id)
+
+        // then
+        untilAsserted {
+            val response = envoy().egressOperations.callService(service = "echo")
+            assertThat(response).isOk().isFrom(redeployedService())
+        }
     }
 
     @Test
     fun `should assign endpoints to correct clusters`() {
         // given
-        registerService(name = "echo")
+        consul().server.operations.registerService(service(), name = "echo")
 
         untilAsserted {
             // when
-            val adminInstance = envoyContainer1.admin().cluster(cluster = "echo", ip = echoContainer.ipAddress())
+            val adminInstance =
+                envoy().container.admin().cluster(cluster = "echo", ip = service().container().ipAddress())
 
             // then
             assertThat(adminInstance).isNotNull
             assertThat(adminInstance!!.cluster).isEqualTo("dc1")
+        }
+    }
+
+    fun redeployEchoService(id: String) {
+        // we first register a new instance and then remove other to maintain cluster presence in Envoy
+        consul().server.operations.registerService(redeployedService(), name = "echo")
+        waitForEchoServices(instances = 2)
+
+        consul().server.operations.deregisterService(id)
+        waitForEchoServices(instances = 1)
+    }
+
+    private fun waitForEchoServices(instances: Int) {
+        untilAsserted {
+            assertThat(envoy().container.admin().numOfEndpoints(clusterName = "echo"))
+                .isEqualTo(instances)
         }
     }
 }
