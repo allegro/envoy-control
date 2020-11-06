95c469a6871210c2c73dfa09b36aedb86b7cee6b
Lukasz Dziedziak <lukasz.dziedziak@allegro.pl>
2020-11-06 11:50:30 +0100

    Fixed produces to work with older tools

--- envoy-control-runner/src/main/kotlin/pl/allegro/tech/servicemesh/envoycontrol/snapshot/debug/SnapshotDebugController.kt
+++ envoy-control-runner/src/main/kotlin/pl/allegro/tech/servicemesh/envoycontrol/snapshot/debug/SnapshotDebugController.kt
@@ -49,7 +49,8 @@ class SnapshotDebugController(controlPlane: ControlPlane) {
      * It contains the versions of XDS resources and the contents for a provided node JSON
      * extracted from Envoy's config_dump endpoint.
      */
-    @PostMapping(value = ["/snapshot"], consumes = ["application/json"], produces = ["application/v2+json"])
+    @PostMapping(value = ["/snapshot"], consumes = ["application/json"], produces = ["application/v2+json",
+        "application/json"])
     fun snapshot(@RequestBody node: NodeV2): ResponseEntity<SnapshotDebugInfo> {
         val nodeHash = nodeGroup.hash(node)
         val snapshot = cache.getSnapshot(nodeHash)
