19a30fe87d1a421a4bc71faccb72dcfcd4e9919d
Lukasz Dziedziak <lukasz.dziedziak@allegro.pl>
2020-12-22 12:45:32 +0100

    Migrate IncomingPermissionsDisabledInClientTest

--- envoy-control-tests/src/main/kotlin/pl/allegro/tech/servicemesh/envoycontrol/permissions/IncomingPermissionsDisabledInClientTest.kt
+++ envoy-control-tests/src/main/kotlin/pl/allegro/tech/servicemesh/envoycontrol/permissions/IncomingPermissionsDisabledInClientTest.kt
@@ -2,38 +2,53 @@ package pl.allegro.tech.servicemesh.envoycontrol.permissions
 
 import okhttp3.Headers
 import org.assertj.core.api.Assertions.assertThat
-import org.junit.jupiter.api.BeforeAll
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.RegisterExtension
+import pl.allegro.tech.servicemesh.envoycontrol.assertions.isFrom
+import pl.allegro.tech.servicemesh.envoycontrol.assertions.isOk
+import pl.allegro.tech.servicemesh.envoycontrol.assertions.untilAsserted
 import pl.allegro.tech.servicemesh.envoycontrol.config.AdsWithDisabledEndpointPermissions
-import pl.allegro.tech.servicemesh.envoycontrol.config.envoycontrol.EnvoyControlRunnerTestApp
-import pl.allegro.tech.servicemesh.envoycontrol.config.EnvoyControlTestConfiguration
+import pl.allegro.tech.servicemesh.envoycontrol.config.consul.ConsulExtension
+import pl.allegro.tech.servicemesh.envoycontrol.config.envoy.EnvoyExtension
+import pl.allegro.tech.servicemesh.envoycontrol.config.envoycontrol.EnvoyControlExtension
+import pl.allegro.tech.servicemesh.envoycontrol.config.service.EchoServiceExtension
 
-internal class IncomingPermissionsDisabledInClientTest : EnvoyControlTestConfiguration() {
+internal class IncomingPermissionsDisabledInClientTest {
 
     companion object {
 
-        private val properties = mapOf(
-            "envoy-control.envoy.snapshot.incoming-permissions.enabled" to true
+        @JvmField
+        @RegisterExtension
+        val consul = ConsulExtension()
+
+        @JvmField
+        @RegisterExtension
+        val envoyControl = EnvoyControlExtension(
+            consul, mapOf(
+                "envoy-control.envoy.snapshot.incoming-permissions.enabled" to true
+            )
         )
 
-        @JvmStatic
-        @BeforeAll
-        fun setupTest() {
-            setup(appFactoryForEc1 = { consulPort ->
-                EnvoyControlRunnerTestApp(properties = properties, consulPort = consulPort)
-            }, envoyConfig = AdsWithDisabledEndpointPermissions)
-        }
+        @JvmField
+        @RegisterExtension
+        val service = EchoServiceExtension()
+
+        @JvmField
+        @RegisterExtension
+        val envoy = EnvoyExtension(envoyControl, localService = service, config = AdsWithDisabledEndpointPermissions)
     }
 
     @Test
     fun `should allow access to endpoint by any client when endpoint permissions not defined in client`() {
         untilAsserted {
             // when
-            val response = callLocalService(endpoint = "/",
-                headers = Headers.of(mapOf("x-service-name" to "any")))
+            val response = envoy.ingressOperations.callLocalService(
+                "/",
+                headers = Headers.of(mapOf("x-service-name" to "any"))
+            )
 
             // then
-            assertThat(response).isOk().isFrom(localServiceContainer)
+            assertThat(response).isOk().isFrom(service)
         }
     }
 }
