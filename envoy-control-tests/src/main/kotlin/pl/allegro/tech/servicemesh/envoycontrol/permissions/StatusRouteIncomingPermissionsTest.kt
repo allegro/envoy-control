ad38c0e3ce477ab8dc84666584a3569441a0c6eb
Łukasz Dziedziak <lukidzi@gmail.com>
2021-01-12 13:05:59 +0100

    Migrate  Tests to new approach (#236)
    * Migrate SnapshotDebugTest to new approach
    
    * Migrate IncomingPermissionsDisbaledInECTest
    
    * Migrate IncomingPermissionsEmptyClientsTest
    
    * Migrate IncomingPermissionsEmptyEndpointsTest
    
    * Migrate StatusRouteIncomingPermissionsTest
    
    * Migrate IncomingPermissionsDisabledInClientTest
    
    Co-authored-by: Lukasz Dziedziak <lukasz.dziedziak@allegro.pl>

--- envoy-control-tests/src/main/kotlin/pl/allegro/tech/servicemesh/envoycontrol/permissions/StatusRouteIncomingPermissionsTest.kt
+++ envoy-control-tests/src/main/kotlin/pl/allegro/tech/servicemesh/envoycontrol/permissions/StatusRouteIncomingPermissionsTest.kt
@@ -1,14 +1,18 @@
 package pl.allegro.tech.servicemesh.envoycontrol.permissions
 
-import okhttp3.Headers
 import org.assertj.core.api.Assertions.assertThat
-import org.junit.jupiter.api.BeforeAll
 import org.junit.jupiter.api.Test
-import pl.allegro.tech.servicemesh.envoycontrol.config.envoycontrol.EnvoyControlRunnerTestApp
-import pl.allegro.tech.servicemesh.envoycontrol.config.EnvoyControlTestConfiguration
+import org.junit.jupiter.api.extension.RegisterExtension
+import pl.allegro.tech.servicemesh.envoycontrol.assertions.isFrom
+import pl.allegro.tech.servicemesh.envoycontrol.assertions.isOk
+import pl.allegro.tech.servicemesh.envoycontrol.assertions.untilAsserted
+import pl.allegro.tech.servicemesh.envoycontrol.config.consul.ConsulExtension
+import pl.allegro.tech.servicemesh.envoycontrol.config.envoy.EnvoyExtension
+import pl.allegro.tech.servicemesh.envoycontrol.config.envoycontrol.EnvoyControlExtension
+import pl.allegro.tech.servicemesh.envoycontrol.config.service.EchoServiceExtension
 import pl.allegro.tech.servicemesh.envoycontrol.snapshot.EndpointMatch
 
-internal class StatusRouteIncomingPermissionsTest : EnvoyControlTestConfiguration() {
+internal class StatusRouteIncomingPermissionsTest {
 
     companion object {
 
@@ -16,30 +20,40 @@ internal class StatusRouteIncomingPermissionsTest : EnvoyControlTestConfiguratio
             "envoy-control.envoy.snapshot.incoming-permissions.enabled" to true,
             "envoy-control.envoy.snapshot.incoming-permissions.overlapping-paths-fix" to true,
             "envoy-control.envoy.snapshot.routes.status.create-virtual-cluster" to true,
-            "envoy-control.envoy.snapshot.routes.status.endpoints" to mutableListOf(EndpointMatch().also { it.path = "/status/" }),
+            "envoy-control.envoy.snapshot.routes.status.endpoints" to mutableListOf(EndpointMatch().also {
+                it.path = "/status/"
+            }),
             "envoy-control.envoy.snapshot.routes.status.enabled" to true
         )
 
-        @JvmStatic
-        @BeforeAll
-        fun setupTest() {
-            setup(appFactoryForEc1 = { consulPort ->
-                EnvoyControlRunnerTestApp(properties = properties, consulPort = consulPort)
-            })
-        }
+        @JvmField
+        @RegisterExtension
+        val consul = ConsulExtension()
+
+        @JvmField
+        @RegisterExtension
+        val envoyControl = EnvoyControlExtension(consul, properties)
+
+        @JvmField
+        @RegisterExtension
+        val service = EchoServiceExtension()
+
+        @JvmField
+        @RegisterExtension
+        val envoy = EnvoyExtension(envoyControl, localService = service)
     }
 
     @Test
     fun `should allow access to status endpoint by all clients`() {
         untilAsserted {
             // when
-            val response = callLocalService("/status/", Headers.of())
-            val statusUpstreamOk = envoyContainer1.admin().statValue(
-                    "vhost.secured_local_service.vcluster.status.upstream_rq_200"
+            val response = envoy.ingressOperations.callLocalService("/status/")
+            val statusUpstreamOk = envoy.container.admin().statValue(
+                "vhost.secured_local_service.vcluster.status.upstream_rq_200"
             )?.toInt()
 
             // then
-            assertThat(response).isOk().isFrom(localServiceContainer)
+            assertThat(response).isOk().isFrom(service)
             assertThat(statusUpstreamOk).isGreaterThan(0)
         }
     }
