abb187f2d5a41e1135210d44050a067ce3754876
slonka <slonka@users.noreply.github.com>
2021-04-12 12:54:48 +0200

    Since using delta mode, cache single messages not whole collections

--- envoy-control-core/src/main/kotlin/pl/allegro/tech/servicemesh/envoycontrol/server/CachedProtoResourcesSerializer.kt
+++ envoy-control-core/src/main/kotlin/pl/allegro/tech/servicemesh/envoycontrol/server/CachedProtoResourcesSerializer.kt
@@ -19,23 +19,10 @@ internal class CachedProtoResourcesSerializer(
     private val reportMetrics: Boolean
 ) : DefaultProtoResourcesSerializer() {
 
-    private fun createTimer(reportMetrics: Boolean, meterRegistry: MeterRegistry, timerName: String): Timer {
-        return if (reportMetrics) {
-            meterRegistry.timer(timerName)
-        } else {
-            noopTimer
-        }
-    }
-
-    private val cacheV2: Cache<Collection<Message>, MutableCollection<Any>> = createCache("protobuf-cache-v2")
-    private val cacheV3: Cache<Collection<Message>, MutableCollection<Any>> = createCache("protobuf-cache-v3")
+    private val cacheV2: Cache<Message, Any> = createCache("protobuf-cache-v2")
+    private val cacheV3: Cache<Message, Any> = createCache("protobuf-cache-v3")
     private val v2Timer = createTimer(reportMetrics, meterRegistry, "protobuf-cache-v2.serialize.time")
     private val v3Timer = createTimer(reportMetrics, meterRegistry, "protobuf-cache-v3.serialize.time")
-
-    private val deltaCacheV2: Cache<Message, Any> = createCache("protobuf-delta-cache-v2")
-    private val deltaCacheV3: Cache<Message, Any> = createCache("protobuf-delta-cache-v3")
-    private val deltaV2Timer = createTimer(reportMetrics, meterRegistry, "protobuf-delta-cache-v2.serialize.time")
-    private val deltaV3Timer = createTimer(reportMetrics, meterRegistry, "protobuf-delta-cache-v3.serialize.time")
 
     private fun <K, V> createCache(cacheName: String): Cache<K, V> {
         return if (reportMetrics) {
@@ -55,49 +42,37 @@ internal class CachedProtoResourcesSerializer(
         }
     }
 
-    override fun serialize(
-        resources: MutableCollection<out Message>,
-        apiVersion: Resources.ApiVersion
-    ): MutableCollection<Any> {
-        val timer = when (apiVersion) {
-            V2 -> v2Timer
-            V3 -> v3Timer
+    private fun createTimer(reportMetrics: Boolean, meterRegistry: MeterRegistry, timerName: String): Timer {
+        return if (reportMetrics) {
+            meterRegistry.timer(timerName)
+        } else {
+            noopTimer
         }
-
-        return timer.record(Supplier { getResources(resources, apiVersion) })
     }
 
-    private fun getResources(
-        resources: MutableCollection<out Message>,
-        apiVersion: Resources.ApiVersion
-    ): MutableCollection<Any> {
+
+    /**
+     * {@inheritDoc}
+     */
+    override fun serialize(resource: Message, apiVersion: Resources.ApiVersion): Any {
         val cache = when (apiVersion) {
             V2 -> cacheV2
             V3 -> cacheV3
         }
 
-        return cache.get(resources) {
-            resources.asSequence()
-                .map { super.maybeRewriteTypeUrl(Any.pack(it), apiVersion) }
-                .toMutableList()
+        val timer = when (apiVersion) {
+            V2 -> v2Timer
+            V3 -> v3Timer
         }
-    }
 
-    // override fun serialize(resource: Message, apiVersion: Resources.ApiVersion): Any {
-    //     val cache = when (apiVersion) {
-    //         V2 -> deltaCacheV2
-    //         V3 -> deltaCacheV3
-    //     }
-    //
-    //     val timer = when (apiVersion) {
-    //         V2 -> deltaV2Timer
-    //         V3 -> deltaV3Timer
-    //     }
-    //
-    //     return timer.record(Supplier {
-    //         cache.get(resource) {
-    //             super.maybeRewriteTypeUrl(Any.pack(resource), apiVersion)
-    //         }
-    //     })
-    // }
+        return timer.record(Supplier {
+            cache.get(resource) {
+                super.maybeRewriteTypeUrl(
+                    Any.pack(
+                        resource
+                    ), apiVersion
+                )
+            }
+        })
+    }
 }
