32c290b33aa69a4f0fb8c9db332579046b21d658
Kornel Skorka <kornel.skorka@allegro.pl>
2021-04-26 13:41:08 +0200

    added tests for Jwt Filter factory

--- envoy-control-core/src/test/kotlin/pl/allegro/tech/servicemesh/envoycontrol/snapshot/resource/listeners/filters/JwtFilterFactoryTest.kt
+++ envoy-control-core/src/test/kotlin/pl/allegro/tech/servicemesh/envoycontrol/snapshot/resource/listeners/filters/JwtFilterFactoryTest.kt
@@ -1,5 +1,9 @@
 package pl.allegro.tech.servicemesh.envoycontrol.snapshot.resource.listeners.filters
 
+import com.google.protobuf.Any
+import com.google.protobuf.util.JsonFormat
+import io.envoyproxy.envoy.extensions.filters.http.jwt_authn.v3.JwtAuthentication
+import io.envoyproxy.envoy.extensions.filters.network.http_connection_manager.v3.HttpFilter
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 import pl.allegro.tech.servicemesh.envoycontrol.groups.CommunicationMode
@@ -57,20 +61,128 @@ internal class JwtFilterFactoryTest {
     @Test
     fun `should create JWT filter`() {
         // given
-        val group: Group = createGroup(mapOf("/" to "provider"))
+        val group = createGroup(mapOf("/" to "provider"))
+        val expectedJwtFilter = getJwtFilter(singleProviderJson)
 
         // when
-        val filter = jwtFilterFactory.createJwtFilter(group)
+        val generatedFilter = jwtFilterFactory.createJwtFilter(group)
 
         // then
-        assertThat(filter).isNotNull
+        assertThat(generatedFilter).isNotNull
+        assertThat(generatedFilter).isEqualTo(expectedJwtFilter)
+    }
+
+    @Test
+    fun `should create JWT filter with multiple providers`() {
+        // given
+        val group = createGroup(
+            mapOf(
+                "/provider1-protected" to "provider1",
+                "/provider2-protected" to "provider2"
+            )
+        )
+        val expectedJwtFilter = getJwtFilter(multiProviderJson)
+
+        // when
+        val generatedFilter = multiProviderJwtFilterFactory.createJwtFilter(group)
+
+        // then
+        assertThat(generatedFilter).isNotNull
+        assertThat(generatedFilter).isEqualTo(expectedJwtFilter)
+    }
+
+    private fun getJwtFilter(providersJson: String): HttpFilter? {
+        val jwt = JwtAuthentication.newBuilder()
+        JsonFormat.parser().merge(providersJson, jwt)
+        return HttpFilter.newBuilder()
+            .setName("envoy.filters.http.jwt_authn")
+            .setTypedConfig(
+                Any.pack(
+                    jwt.build()
+                )
+            )
+            .build()
     }
 
     private fun createGroup(pathToProvider: Map<String, String>) = ServicesGroup(
         CommunicationMode.ADS, proxySettings = ProxySettings(
             Incoming(
-                pathToProvider.map { (path, provider) -> IncomingEndpoint(path, oauth = OAuth(provider)) }
+                pathToProvider.map { (path, provider) -> IncomingEndpoint(path, oauth = OAuth(provider, policy = OAuth.Policy.STRICT)) }
             )
         )
     )
+
+    private val singleProviderJson = """{
+  "providers": {
+    "provider": {
+      "issuer": "provider",
+      "remoteJwks": {
+        "httpUri": {
+          "uri": "http://provider/jwks",
+          "cluster": "provider-cluster",
+          "timeout": "1s"
+        },
+        "cacheDuration": "300s"
+      },
+      "forward": true,
+      "forwardPayloadHeader": "x-oauth-token-validated",
+      "payloadInMetadata": "jwt"
+    }
+  },
+  "rules": [{
+    "match": {
+      "prefix": "/"
+    },
+    "requires": {
+      "providerName": "provider"
+    }
+  }]
+}"""
+    private val multiProviderJson = """{
+  "providers": {
+    "provider1": {
+      "issuer": "provider1",
+      "remoteJwks": {
+        "httpUri": {
+          "uri": "http://provider1/jwks",
+          "cluster": "provider1-cluster",
+          "timeout": "1s"
+        },
+        "cacheDuration": "300s"
+      },
+      "forward": true,
+      "forwardPayloadHeader": "x-oauth-token-validated",
+      "payloadInMetadata": "jwt"
+    },
+    "provider2": {
+      "issuer": "provider2",
+      "remoteJwks": {
+        "httpUri": {
+          "uri": "http://provider2/jwks",
+          "cluster": "provider2-cluster",
+          "timeout": "1s"
+        },
+        "cacheDuration": "300s"
+      },
+      "forward": true,
+      "forwardPayloadHeader": "x-oauth-token-validated",
+      "payloadInMetadata": "jwt"
+    }
+  },
+  "rules": [{
+    "match": {
+      "prefix": "/provider1-protected"
+    },
+    "requires": {
+      "providerName": "provider1"
+    }
+  }, {
+    "match": {
+      "prefix": "/provider2-protected"
+    },
+    "requires": {
+      "providerName": "provider2"
+    }
+  }]
+}"""
 }
