609b0ca6c87b8eb939b9a7cf5e0e4388492f2670
Lukasz Dziedziak <lukasz.dziedziak@allegro.pl>
2021-05-25 15:11:49 -0500

    Test

--- envoy-control-core/src/main/kotlin/pl/allegro/tech/servicemesh/envoycontrol/snapshot/resource/listeners/EnvoyListenersFactory.kt
+++ envoy-control-core/src/main/kotlin/pl/allegro/tech/servicemesh/envoycontrol/snapshot/resource/listeners/EnvoyListenersFactory.kt
@@ -565,7 +565,9 @@ class EnvoyListenersFactory(
         val filterChainMatch = FilterChainMatch.newBuilder()
         val filter = Filter.newBuilder()
         filterChainMatch.setTransportProtocol("raw_buffer")
-            .setDestinationPort(UInt32Value.of(80))
+            .addApplicationProtocols("http/1.0")
+            .addApplicationProtocols("http/1.1")
+            .addApplicationProtocols("h2")
         filter
             .setName("envoy.filters.network.http_connection_manager")
             .setTypedConfig(
