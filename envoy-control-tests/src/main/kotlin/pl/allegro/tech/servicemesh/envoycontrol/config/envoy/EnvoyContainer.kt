5f3f34dcb419a24d9a3e7f2bbca85f2ecd332843
Kamil Smigielski <kamil.smigielski@allegro.pl>
2022-01-17 12:39:38 +0100

    set envoy version to v1.20.1 and remove flag --bootstrap-version

--- envoy-control-tests/src/main/kotlin/pl/allegro/tech/servicemesh/envoycontrol/config/envoy/EnvoyContainer.kt
+++ envoy-control-tests/src/main/kotlin/pl/allegro/tech/servicemesh/envoycontrol/config/envoy/EnvoyContainer.kt
@@ -17,13 +17,12 @@ class EnvoyContainer(
     private val envoyControl2XdsPort: Int = envoyControl1XdsPort,
     private val logLevel: String = "info",
     image: String = DEFAULT_IMAGE,
-    private val apiVersion: Int = 3
 ) : SSLGenericContainer<EnvoyContainer>(
     dockerfileBuilder = DockerfileBuilder()
         .from(image)
         .run("apt-get update && apt-get install -y curl iproute2 iptables dnsmasq")
         .run("adduser --disabled-password --gecos \"\" test")
-
+) {
 
     companion object {
         val logger by loggerDelegate()
