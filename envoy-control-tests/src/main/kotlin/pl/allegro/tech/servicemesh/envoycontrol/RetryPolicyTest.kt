497fbc59bf74c036590c3cb1b4db1b17bd4758c7
malfoj89 <lukasz.malewski@allegro.pl>
2022-02-08 12:29:56 +0100

    retry mechanizm at routes - failing test

--- envoy-control-tests/src/main/kotlin/pl/allegro/tech/servicemesh/envoycontrol/RetryPolicyTest.kt
+++ envoy-control-tests/src/main/kotlin/pl/allegro/tech/servicemesh/envoycontrol/RetryPolicyTest.kt
@@ -4,14 +4,30 @@ import okhttp3.Headers.Companion.toHeaders
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.RegisterExtension
+import pl.allegro.tech.servicemesh.envoycontrol.assertions.isFrom
+import pl.allegro.tech.servicemesh.envoycontrol.assertions.isOk
 import pl.allegro.tech.servicemesh.envoycontrol.assertions.untilAsserted
+import pl.allegro.tech.servicemesh.envoycontrol.config.Xds
 import pl.allegro.tech.servicemesh.envoycontrol.config.envoycontrol.EnvoyControlExtension
 import pl.allegro.tech.servicemesh.envoycontrol.config.consul.ConsulExtension
 import pl.allegro.tech.servicemesh.envoycontrol.config.service.EchoServiceExtension
 import pl.allegro.tech.servicemesh.envoycontrol.config.envoy.EnvoyExtension
+import java.time.Duration
 
 class RetryPolicyTest {
     companion object {
+
+        private val RETRY_SETTINGS_CONFIG = """
+node:
+  metadata:
+    proxy_settings:
+      outgoing:
+        dependencies:
+          - service: "echo"
+            retryPolicy:
+              numberRetries: "8"
+              retryableStatusCodes: ["200"]
+            """.trimIndent()
 
         @JvmField
         @RegisterExtension
@@ -19,11 +35,16 @@ class RetryPolicyTest {
 
         @JvmField
         @RegisterExtension
-        val envoyControl = EnvoyControlExtension(consul, mapOf(
+        val envoyControl = EnvoyControlExtension(
+            consul, mapOf(
                 "envoy-control.envoy.snapshot.local-service.retry-policy.per-http-method.GET.enabled" to true,
-                "envoy-control.envoy.snapshot.local-service.retry-policy.per-http-method.GET.retry-on" to listOf("connect-failure", "reset"),
+                "envoy-control.envoy.snapshot.local-service.retry-policy.per-http-method.GET.retry-on" to listOf(
+                    "connect-failure",
+                    "reset"
+                ),
                 "envoy-control.envoy.snapshot.local-service.retry-policy.per-http-method.GET.num-retries" to 3
-        ))
+            )
+        )
 
         @JvmField
         @RegisterExtension
@@ -31,7 +52,7 @@ class RetryPolicyTest {
 
         @JvmField
         @RegisterExtension
-        val envoy = EnvoyExtension(envoyControl, service)
+        val envoy = EnvoyExtension(envoyControl, service, config = Xds.copy(configOverride = RETRY_SETTINGS_CONFIG))
     }
 
     @Test
@@ -46,13 +67,30 @@ class RetryPolicyTest {
 
         untilAsserted {
             // then
-            assertThat(hasRetriedRequest(numberOfRetries = 3)).isTrue()
+            assertThat(hasRetriedRequest(numberOfRetries = 3, metricName = "cluster.local_service.upstream_rq_retry")).isTrue()
         }
     }
 
-    private fun hasRetriedRequest(numberOfRetries: Long): Boolean {
+
+    @Test
+    fun `should retry 8 times when configured retry policy route for service`(){
+        consul.server.operations.registerService(service, name = "echo")
+
+        untilAsserted {
+            val response = envoy.egressOperations.callService("echo")
+            assertThat(response).isOk().isFrom(service)
+        }
+
+        untilAsserted(wait = Duration.ofSeconds(5)) {
+            // then
+            //assertThat(hasRetriedRequest(numberOfRetries = 8, metricName = "vhost.service-1.vcluster.other.upstream_rq_retry")).isTrue()
+            assertThat(envoy.container.admin().statValue("vhost.echo.vcluster.other.upstream_rq_retry")!!.toLong()).isEqualTo(8)
+        }
+    }
+
+    private fun hasRetriedRequest(numberOfRetries: Long, metricName: String): Boolean {
         return envoy.container.admin()
-            .statValue("cluster.local_service.upstream_rq_retry")
+            .statValue(metricName)
             ?.toLong()
             ?.equals(numberOfRetries)
             ?: false
