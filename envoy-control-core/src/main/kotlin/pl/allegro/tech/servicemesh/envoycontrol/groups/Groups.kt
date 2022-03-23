cafb647a73afe108d9410ceb780a5ae8beb509a7
radoslaw.chrzanowski <radoslaw.chrzanowski@allegro.pl>
2022-03-23 15:05:56 +0100

    remove resources_dir from listeners hashCode/equals

--- envoy-control-core/src/main/kotlin/pl/allegro/tech/servicemesh/envoycontrol/groups/Groups.kt
+++ envoy-control-core/src/main/kotlin/pl/allegro/tech/servicemesh/envoycontrol/groups/Groups.kt
@@ -24,6 +24,7 @@ data class AllServicesGroup(
     override val listenersConfig: ListenersConfig? = null
 ) : Group()
 
+@SuppressWarnings("ComplexMethod")
 data class ListenersConfig(
     val ingressHost: String,
     val ingressPort: Int,
@@ -53,5 +54,47 @@ data class ListenersConfig(
         const val defaultResourcesDir = "envoy"
         const val defaultHasStaticSecretsDefined: Boolean = false
         const val defaultUseTransparentProxy: Boolean = false
+    }
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as ListenersConfig
+
+        if (ingressHost != other.ingressHost) return false
+        if (ingressPort != other.ingressPort) return false
+        if (egressHost != other.egressHost) return false
+        if (egressPort != other.egressPort) return false
+        if (useRemoteAddress != other.useRemoteAddress) return false
+        if (generateRequestId != other.generateRequestId) return false
+        if (preserveExternalRequestId != other.preserveExternalRequestId) return false
+        if (accessLogEnabled != other.accessLogEnabled) return false
+        if (enableLuaScript != other.enableLuaScript) return false
+        if (accessLogPath != other.accessLogPath) return false
+        if (addUpstreamExternalAddressHeader != other.addUpstreamExternalAddressHeader) return false
+        if (accessLogFilterSettings != other.accessLogFilterSettings) return false
+        if (hasStaticSecretsDefined != other.hasStaticSecretsDefined) return false
+        if (useTransparentProxy != other.useTransparentProxy) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = ingressHost.hashCode()
+        result = 31 * result + ingressPort
+        result = 31 * result + egressHost.hashCode()
+        result = 31 * result + egressPort
+        result = 31 * result + useRemoteAddress.hashCode()
+        result = 31 * result + generateRequestId.hashCode()
+        result = 31 * result + preserveExternalRequestId.hashCode()
+        result = 31 * result + accessLogEnabled.hashCode()
+        result = 31 * result + enableLuaScript.hashCode()
+        result = 31 * result + accessLogPath.hashCode()
+        result = 31 * result + addUpstreamExternalAddressHeader.hashCode()
+        result = 31 * result + accessLogFilterSettings.hashCode()
+        result = 31 * result + hasStaticSecretsDefined.hashCode()
+        result = 31 * result + useTransparentProxy.hashCode()
+        return result
     }
 }
