9bb79f2530889a44cb36db7813b29eacb08d977b
Kamil Smigielski <kamil.smigielski@allegro.pl>
2022-08-26 12:26:03 +0200

    add debug endpoint by service name

--- envoy-control-runner/src/main/kotlin/pl/allegro/tech/servicemesh/envoycontrol/snapshot/debug/GlobalSnapshotInfo.kt
+++
@@ -1,5 +0,0 @@
-package pl.allegro.tech.servicemesh.envoycontrol.snapshot.debug
-
-data class GlobalSnapshotInfo(val clusters: List<EndpointInfo> = emptyList())
-
-data class EndpointInfo(val datacenter: String, val ip: String, val port: Int)
