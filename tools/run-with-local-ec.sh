d60327abd879d7cf51aecb67cd4300558f834c23
Wiktor Czajkowski <wiktor.czajkowski@allegro.pl>
2022-12-05 09:51:29 +0100

    WIP | Add flask-service and shorter services names

--- tools/run-with-local-ec.sh
+++ tools/run-with-local-ec.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-docker-compose up --no-deps --build consul front-proxy jaeger service1 service2 service3 tracing flask-service
+docker-compose up --no-deps --build consul front-proxy jaeger s1 s2 s3 tracing flask-service
