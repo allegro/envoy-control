c5ed9af5ec49907627a2c6473232b487ea38a3e0
nastassia.dailidava <nastassia.dailidava@allegrogroup.com>
2023-05-19 15:09:41 +0200

    Implemented compressing response body that sent during cross-dc synchronization #270 (fixed tests, added header)

--- envoy-control-runner/src/main/kotlin/pl/allegro/tech/servicemesh/envoycontrol/synchronization/GzipHttpRequestInterceptor.kt
+++ envoy-control-runner/src/main/kotlin/pl/allegro/tech/servicemesh/envoycontrol/synchronization/GzipHttpRequestInterceptor.kt
@@ -10,11 +10,16 @@ import java.io.InputStream
 import java.util.zip.GZIPInputStream
 
 class GzipHttpRequestInterceptor : ClientHttpRequestInterceptor {
+    companion object {
+        const val HEADER_GZIP = "gzip"
+    }
+
     override fun intercept(
         request: HttpRequest,
         body: ByteArray,
         execution: ClientHttpRequestExecution
     ): ClientHttpResponse {
+        request.headers.add(HttpHeaders.CONTENT_ENCODING, HEADER_GZIP)
         val gzipResponse = execution.execute(request, body)
         return object : ClientHttpResponse {
             override fun getHeaders(): HttpHeaders = gzipResponse.headers
--- envoy-control-tests/src/main/kotlin/pl/allegro/tech/servicemesh/envoycontrol/StateControllerTest.kt
+++ envoy-control-tests/src/main/kotlin/pl/allegro/tech/servicemesh/envoycontrol/StateControllerTest.kt
@@ -1,40 +1,40 @@
 package pl.allegro.tech.servicemesh.envoycontrol
 
-import org.assertj.core.api.Assertions
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.module.kotlin.KotlinModule
+import okhttp3.Request
+import okhttp3.Response
+import org.assertj.core.api.Assertions.assertThat
 import org.awaitility.Awaitility
+import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.TestInstance
 import org.junit.jupiter.api.extension.RegisterExtension
-import org.junit.jupiter.params.ParameterizedTest
-import org.junit.jupiter.params.provider.MethodSource
+import org.springframework.http.HttpHeaders
+import org.springframework.http.MediaType.APPLICATION_JSON_VALUE
 import pl.allegro.tech.servicemesh.envoycontrol.assertions.untilAsserted
+import pl.allegro.tech.servicemesh.envoycontrol.config.ClientsFactory
 import pl.allegro.tech.servicemesh.envoycontrol.config.consul.ConsulExtension
+import pl.allegro.tech.servicemesh.envoycontrol.config.envoy.HttpResponseCloser.addToCloseableResponses
 import pl.allegro.tech.servicemesh.envoycontrol.config.envoycontrol.EnvoyControlRunnerTestApp
 import pl.allegro.tech.servicemesh.envoycontrol.config.envoycontrol.EnvoyControlTestApp
+import pl.allegro.tech.servicemesh.envoycontrol.services.ServicesState
 import java.time.Duration
 import java.util.concurrent.TimeUnit
-import java.util.stream.Stream
+import java.util.zip.GZIPInputStream
 
 @TestInstance(TestInstance.Lifecycle.PER_METHOD)
 class StateControllerTest {
     private val logger by logger()
+
+    private val objectMapper = ObjectMapper().registerModule(KotlinModule.Builder().build())
 
     companion object {
         @JvmField
         @RegisterExtension
         val consul = ConsulExtension()
 
-        private val regularProperties = mapOf(
-            "envoy-control.sync.enabled" to true,
-            "envoy-control.envoy.snapshot.routing.service-tags.enabled" to true,
-            "envoy-control.envoy.snapshot.routing.service-tags.metadata-key" to "tag",
-            "envoy-control.envoy.snapshot.routing.service-tags.auto-service-tag-enabled" to true,
-            "envoy-control.envoy.snapshot.routing.service-tags.reject-requests-with-duplicated-auto-service-tag" to true
-        )
-
-        private val gzipEnabledProperties = mapOf(
-            "server.compression.enabled" to true,
-            "server.compression.mime-types" to
-                "application/json,application/xml,text/html,text/xml,text/plain,application/javascript,text/css",
+        @JvmStatic
+        private fun regularProperties() = mapOf(
             "envoy-control.sync.enabled" to true,
             "envoy-control.envoy.snapshot.routing.service-tags.enabled" to true,
             "envoy-control.envoy.snapshot.routing.service-tags.metadata-key" to "tag",
@@ -43,33 +43,80 @@ class StateControllerTest {
         )
 
         @JvmStatic
-        fun provideProperties(): Stream<Map<String, Any>> = Stream.of(
-            regularProperties, gzipEnabledProperties
+        private fun gzipEnabledProperties() = mapOf(
+            "server.compression.enabled" to true,
+            "server.compression.mime-types" to
+                "application/json,application/xml,text/html,text/xml,text/plain,application/javascript,text/css",
+            "server.compression.min-response-size" to "1",
+            "envoy-control.sync.enabled" to true,
+            "envoy-control.envoy.snapshot.routing.service-tags.enabled" to true,
+            "envoy-control.envoy.snapshot.routing.service-tags.metadata-key" to "tag",
+            "envoy-control.envoy.snapshot.routing.service-tags.auto-service-tag-enabled" to true,
+            "envoy-control.envoy.snapshot.routing.service-tags.reject-requests-with-duplicated-auto-service-tag" to true
         )
     }
 
-    @ParameterizedTest
-    @MethodSource(value = ["provideProperties"])
-    fun `should return state`(properties: Map<String, Any>) {
-        logger.info("Running test with parameters: $properties")
+    @Test
+    fun `should return json state`() {
         val app = EnvoyControlRunnerTestApp(
-            propertiesProvider = { properties },
+            propertiesProvider = { regularProperties() },
             consulPort = consul.server.port
         )
         app.run()
         waitUntilHealthy(app)
         untilAsserted(wait = Duration.ofSeconds(5)) {
-            val state = app.getState()
-            Assertions.assertThat(state)
+            val stateResponse = getState(app.appPort)
+            assertThat(stateResponse.body)
+                .hasFieldOrPropertyWithValue("contentTypeString", APPLICATION_JSON_VALUE)
+            val converted =
+                objectMapper.convertValue(stateResponse.body?.byteStream(), ServicesState::class.java)
+            assertThat(converted)
                 .isNotNull()
                 .hasNoNullFieldsOrProperties()
         }
         app.stop()
     }
 
+    @Test
+    fun `should return gzip state`() {
+        val app = buildEnvoyAppWithProperties(gzipEnabledProperties())
+        app.run()
+        waitUntilHealthy(app)
+        untilAsserted(wait = Duration.ofSeconds(5)) {
+            val stateResponse = getState(app.appPort)
+            val unGzippedStr = GZIPInputStream(stateResponse.body?.byteStream())
+                .bufferedReader(Charsets.UTF_8)
+                .use { it.readText() }
+            val converted = objectMapper.readValue(unGzippedStr, ServicesState::class.java)
+            assertThat(converted)
+                .isNotNull()
+                .hasNoNullFieldsOrProperties()
+        }
+        app.stop()
+    }
+
+    private fun buildEnvoyAppWithProperties(properties: Map<String, Any>) =
+        EnvoyControlRunnerTestApp(
+            propertiesProvider = { properties },
+            consulPort = consul.server.port
+        )
+
+    private fun getState(appPort: Int): Response {
+        return ClientsFactory.createClient()
+            .newBuilder().build()
+            .newCall(
+                Request.Builder()
+                    .get()
+                    .url("http://localhost:$appPort/state")
+                    .addHeader(HttpHeaders.ACCEPT_ENCODING, "gzip") //todo optional header
+                    .build()
+            )
+            .execute().addToCloseableResponses()
+    }
+
     private fun waitUntilHealthy(app: EnvoyControlTestApp) {
         Awaitility.await().atMost(30, TimeUnit.SECONDS).untilAsserted {
-            Assertions.assertThat(app.isHealthy()).isTrue()
+            assertThat(app.isHealthy()).isTrue()
         }
     }
 }
