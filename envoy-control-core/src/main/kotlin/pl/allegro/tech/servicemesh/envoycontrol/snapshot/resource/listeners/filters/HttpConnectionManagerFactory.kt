d6481defd57b48300a3c1e519c613433d3fa8acb
Radek Chrzanowski <radoslaw.chrzanowski@allegro.pl>
2023-12-22 14:22:45 +0100

    Normalize path (#405)
    * normalize path
    
    * only merge
    
    * Revert "block some status endpoints (#403)"
    
    This reverts commit 7558272d90ae548ece61860978ce7d871d30ce52.

--- envoy-control-core/src/main/kotlin/pl/allegro/tech/servicemesh/envoycontrol/snapshot/resource/listeners/filters/HttpConnectionManagerFactory.kt
+++ envoy-control-core/src/main/kotlin/pl/allegro/tech/servicemesh/envoycontrol/snapshot/resource/listeners/filters/HttpConnectionManagerFactory.kt
@@ -66,7 +66,6 @@ class HttpConnectionManagerFactory(
                     .setUseRemoteAddress(BoolValue.newBuilder().setValue(listenersConfig.useRemoteAddress).build())
                     .setDelayedCloseTimeout(Duration.newBuilder().setSeconds(0).build())
                     .setCommonHttpProtocolOptions(httpProtocolOptions)
-                    .setNormalizePath(BoolValue.newBuilder().setValue(true).build())
                     .setMergeSlashes(true)
                     .setCodecType(HttpConnectionManager.CodecType.AUTO)
                     .setHttpProtocolOptions(ingressHttp1ProtocolOptions(group.serviceName))
