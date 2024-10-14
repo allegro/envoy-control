3a8608df21689441caca2acf9c2c6f5417f06b2b
nastassia.dailidava <nastassia.dailidava@allegrogroup.com>
2024-10-14 23:30:10 +0200

    allegro-internal/flex-roadmap#819 changed name

--- envoy-control-core/src/main/kotlin/pl/allegro/tech/servicemesh/envoycontrol/utils/Metrics.kt
+++ envoy-control-core/src/main/kotlin/pl/allegro/tech/servicemesh/envoycontrol/utils/Metrics.kt
@@ -6,10 +6,10 @@ import io.micrometer.core.instrument.noop.NoopTimer
 
 val noopTimer = NoopTimer(Meter.Id("", Tags.empty(), null, null, Meter.Type.TIMER))
 const val REACTOR_METRIC = "reactor.pipeline"
-const val ERRORS_TOTAL_METRIC = "errors.total"
-const val CONNECTIONS_METRIC = "connections"
-const val REQUESTS_METRIC = "requests.total"
-const val WATCH_METRIC = "watch"
+const val ERRORS_TOTAL_METRIC = "suffix.errors.total"
+const val CONNECTIONS_METRIC = "suffix.connections"
+const val REQUESTS_METRIC = "suffix.requests.total"
+const val WATCH_METRIC = "suffix.watch"
 const val ENVOY_CONTROL_WARM_UP_METRIC = "envoy.control.warmup.seconds"
 const val CROSS_DC_SYNC_METRIC = "cross.dc.synchronization"
 const val CROSS_DC_SYNC_CANCELLED_METRIC = "$CROSS_DC_SYNC_METRIC.cancelled.total"
@@ -17,7 +17,7 @@ const val CROSS_DC_SYNC_SECONDS_METRIC = "$CROSS_DC_SYNC_METRIC.seconds"
 const val CROSS_DC_SYNC_TOTAL_METRIC = "$CROSS_DC_SYNC_METRIC.total"
 const val SIMPLE_CACHE_METRIC = "simple.cache.duration.seconds"
 const val PROTOBUF_CACHE_METRIC = "protobuf.cache.serialize.time"
-const val CACHE_GROUP_COUNT_METRIC = "cache.groups.count"
+const val CACHE_GROUP_COUNT_METRIC = "suffix.cache.groups.count"
 const val SNAPSHOT_FACTORY_SECONDS_METRIC = "snapshot.factory.seconds"
 
 const val CONNECTION_TYPE_TAG = "connection-type"
