7ba3f47826439db688f2e8794a441d0883e70fee
Kamil Smigielski <kamil.smigielski@allegro.com>
2025-01-31 23:08:05 +0100

    test

--- envoy-control-core/src/main/kotlin/pl/allegro/tech/servicemesh/envoycontrol/groups/GroupChangeWatcher.kt
+++ envoy-control-core/src/main/kotlin/pl/allegro/tech/servicemesh/envoycontrol/groups/GroupChangeWatcher.kt
@@ -4,10 +4,12 @@ import io.envoyproxy.controlplane.cache.ConfigWatcher
 import io.envoyproxy.controlplane.cache.DeltaResponse
 import io.envoyproxy.controlplane.cache.DeltaWatch
 import io.envoyproxy.controlplane.cache.DeltaXdsRequest
+import io.envoyproxy.controlplane.cache.Resources
 import io.envoyproxy.controlplane.cache.Response
 import io.envoyproxy.controlplane.cache.Watch
 import io.envoyproxy.controlplane.cache.XdsRequest
 import io.micrometer.core.instrument.MeterRegistry
+import io.micrometer.core.instrument.Timer
 import pl.allegro.tech.servicemesh.envoycontrol.EnvoyControlMetrics
 import pl.allegro.tech.servicemesh.envoycontrol.logger
 import pl.allegro.tech.servicemesh.envoycontrol.utils.CHANGE_WATCHER_METRIC
@@ -31,7 +33,75 @@ internal class GroupChangeWatcher(
 ) : ConfigWatcher {
     private val groupsChanged: Flux<List<Group>> = Flux.create { groupChangeEmitter = it }
     private var groupChangeEmitter: FluxSink<List<Group>>? = null
+    //             []float64{.01, .1, 1, 3, 5, 10, 20, 30},
+    private val pushTimeEds = Timer.builder("envoy_control_create_delta_watch")
+    .serviceLevelObjectives(
+    java.time.Duration.ofMillis(10),
+    java.time.Duration.ofMillis(100),
+    java.time.Duration.ofSeconds(1),
+    java.time.Duration.ofSeconds(3),
+    java.time.Duration.ofSeconds(5),
+    java.time.Duration.ofSeconds(10),
+    java.time.Duration.ofSeconds(20),
+    java.time.Duration.ofSeconds(30)
+    )
+    .tags("type", "eds")
+    .register(meterRegistry)
 
+private val pushTimeRds = io.micrometer.core.instrument.Timer.builder("envoy_control_create_delta_watch")
+    .serviceLevelObjectives(
+    java.time.Duration.ofMillis(10),
+    java.time.Duration.ofMillis(100),
+    java.time.Duration.ofSeconds(1),
+    java.time.Duration.ofSeconds(3),
+    java.time.Duration.ofSeconds(5),
+    java.time.Duration.ofSeconds(10),
+    java.time.Duration.ofSeconds(20),
+    java.time.Duration.ofSeconds(30)
+    )
+    .tags("type", "rds")
+    .register(meterRegistry)
+
+    private val pushTimeCds = io.micrometer.core.instrument.Timer.builder("envoy_control_create_delta_watch")
+    .serviceLevelObjectives(
+    java.time.Duration.ofMillis(10),
+    java.time.Duration.ofMillis(100),
+    java.time.Duration.ofSeconds(1),
+    java.time.Duration.ofSeconds(3),
+    java.time.Duration.ofSeconds(5),
+    java.time.Duration.ofSeconds(10),
+    java.time.Duration.ofSeconds(20),
+    java.time.Duration.ofSeconds(30)
+    )
+    .tags("type", "cds")
+    .register(meterRegistry)
+
+    private val pushTimeLds = io.micrometer.core.instrument.Timer.builder("envoy_control_create_delta_watch")
+    .serviceLevelObjectives(
+    java.time.Duration.ofMillis(10),
+    java.time.Duration.ofMillis(100),
+    java.time.Duration.ofSeconds(1),
+    java.time.Duration.ofSeconds(3),
+    java.time.Duration.ofSeconds(5),
+    java.time.Duration.ofSeconds(10),
+    java.time.Duration.ofSeconds(20),
+    java.time.Duration.ofSeconds(30)
+    )
+    .tags("type", "lds")
+    .register(meterRegistry)
+    private val pushTimeUnknown = io.micrometer.core.instrument.Timer.builder("envoy_control_create_delta_watch")
+    .serviceLevelObjectives(
+    java.time.Duration.ofMillis(10),
+    java.time.Duration.ofMillis(100),
+    java.time.Duration.ofSeconds(1),
+    java.time.Duration.ofSeconds(3),
+    java.time.Duration.ofSeconds(5),
+    java.time.Duration.ofSeconds(10),
+    java.time.Duration.ofSeconds(20),
+    java.time.Duration.ofSeconds(30)
+    )
+    .tags("type", "unknown")
+    .register(meterRegistry)
     private val logger by logger()
 
     fun onGroupAdded(): Flux<List<Group>> {
@@ -83,27 +153,36 @@ internal class GroupChangeWatcher(
         isWildcard: Boolean,
         responseConsumer: Consumer<DeltaResponse>?,
         hasClusterChanged: Boolean
-    ): DeltaWatch {
-        val oldGroups = cache.groups()
+    ): DeltaWatch =
+        getTimer(request!!).record<DeltaWatch> { val oldGroups = cache.groups()
+            val watch = cache.createDeltaWatch(
+                request,
+                requesterVersion,
+                resourceVersions,
+                pendingResources,
+                isWildcard,
+                responseConsumer,
+                hasClusterChanged
+            )
+            val groups = cache.groups()
+            metrics.setCacheGroupsCount(groups.size)
+            if (oldGroups != groups) {
+                emitNewGroupsEvent(groups - oldGroups)
+            }
+            watch
+        }!!
 
-        val watch = cache.createDeltaWatch(
-            request,
-            requesterVersion,
-            resourceVersions,
-            pendingResources,
-            isWildcard,
-            responseConsumer,
-            hasClusterChanged
-        )
-        val groups = cache.groups()
-        metrics.setCacheGroupsCount(groups.size)
-        if (oldGroups != groups) {
-            emitNewGroupsEvent(groups - oldGroups)
-        }
-        return watch
-    }
 
     private fun emitNewGroupsEvent(difference: List<Group>) {
         groupChangeEmitter?.next(difference)
     }
+
+    fun getTimer(request: DeltaXdsRequest): Timer = when (request.resourceType) {
+        Resources.ResourceType.ENDPOINT -> pushTimeEds
+        Resources.ResourceType.ROUTE -> pushTimeRds
+        Resources.ResourceType.CLUSTER -> pushTimeCds
+        Resources.ResourceType.LISTENER -> pushTimeLds
+        else -> pushTimeUnknown
+    }
+
 }
