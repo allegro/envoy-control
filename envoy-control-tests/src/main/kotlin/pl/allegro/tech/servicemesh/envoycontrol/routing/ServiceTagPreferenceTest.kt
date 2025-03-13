ae6a1d5a54f0fbd6fc32b9a4881de72a09d8ca50
Marcin Falkowski <marcin.falkowski@allegro.com>
2025-03-13 21:37:19 +0100

    PROM-5262 | x-service-tag-preference testing WIP: new tests

--- envoy-control-tests/src/main/kotlin/pl/allegro/tech/servicemesh/envoycontrol/routing/ServiceTagPreferenceTest.kt
+++ envoy-control-tests/src/main/kotlin/pl/allegro/tech/servicemesh/envoycontrol/routing/ServiceTagPreferenceTest.kt
@@ -2,23 +2,33 @@ package pl.allegro.tech.servicemesh.envoycontrol.routing
 
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.BeforeAll
+import org.junit.jupiter.api.ClassOrderer.OrderAnnotation
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.Order
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.TestClassOrder
 import org.junit.jupiter.api.extension.RegisterExtension
 import pl.allegro.tech.servicemesh.envoycontrol.config.consul.ConsulExtension
 import pl.allegro.tech.servicemesh.envoycontrol.config.envoy.CallStats
 import pl.allegro.tech.servicemesh.envoycontrol.config.envoy.EnvoyExtension
 import pl.allegro.tech.servicemesh.envoycontrol.config.envoycontrol.EnvoyControlExtension
 import pl.allegro.tech.servicemesh.envoycontrol.config.service.EchoServiceExtension
+import pl.allegro.tech.servicemesh.envoycontrol.config.service.ServiceExtension
 import pl.allegro.tech.servicemesh.envoycontrol.config.service.UpstreamService
 import pl.allegro.tech.servicemesh.envoycontrol.config.sharing.ContainerExtension
+import pl.allegro.tech.servicemesh.envoycontrol.routing.ServiceTagPreferenceTest.Extensions.assertAllResponsesOkAndFrom
+import pl.allegro.tech.servicemesh.envoycontrol.routing.ServiceTagPreferenceTest.Extensions.callServiceRepeatedly
+import pl.allegro.tech.servicemesh.envoycontrol.routing.ServiceTagPreferenceTest.Extensions.deregisterInstance
+import pl.allegro.tech.servicemesh.envoycontrol.routing.ServiceTagPreferenceTest.Extensions.registerInstance
 
+@TestClassOrder(OrderAnnotation::class)
 class ServiceTagPreferenceTest {
 
     companion object {
 
         val properties = mapOf(
             "envoy-control.envoy.snapshot.routing.service-tags.enabled" to true,
-            "envoy-control.envoy.snapshot.routing.service-tags.auto-service-tag-enabled" to true,  // TODO: testing with false?
+            "envoy-control.envoy.snapshot.routing.service-tags.auto-service-tag-enabled" to true, // TODO: testing with false?
             "envoy-control.envoy.snapshot.routing.service-tags.add-upstream-service-tags-header" to true,
             "envoy-control.envoy.snapshot.routing.service-tags.preference-routing.header" to "x-service-tag-preference",
             "envoy-control.envoy.snapshot.routing.service-tags.preference-routing.default-preference-env" to "DEFAULT_SERVICE_TAG_PREFERENCE",
@@ -63,65 +73,90 @@ class ServiceTagPreferenceTest {
         val echoVte33 = EchoServiceExtension()
 
         val allServices = listOf(echoGlobal, echoVte12, echoVte12Lvte1, echoVte33)
+    }
 
-        @JvmStatic
-        @BeforeAll
-        fun registerServices() {
-            val consulOps = consul.server.operations
-            consulOps.registerService(name = "echo", extension = echoGlobal, tags = listOf("global", "other-1"))
-            consulOps.registerService(name = "echo", extension = echoVte12, tags = listOf("vte12", "other-2"))
-            consulOps.registerService(name = "echo", extension = echoVte12Lvte1, tags = listOf("lvte1", "other-3"))
-            consulOps.registerService(name = "echo", extension = echoVte33, tags = listOf("vte33", "other-3"))
+    open class WhenAllServicesAreUpSetup {
+        companion object {
+            @JvmStatic
+            @BeforeAll
+            fun registerServices() {
+                registerInstance(name = "echo", tags = listOf("global", "other-1"), echoGlobal)
+                registerInstance(name = "echo", tags = listOf("vte12", "other-2"), echoVte12)
+                registerInstance(name = "echo", tags = listOf("lvte1", "other-3"), echoVte12Lvte1)
+                registerInstance(name = "echo", tags = listOf("vte33", "other-3"), echoVte33)
 
-            listOf(envoyGlobal, envoyVte12, envoyVte12Lvte1).forEach { envoy ->
-                allServices.forEach { service ->
-                    envoy.waitForClusterEndpointHealthy("echo", service.container().ipAddress())
+                listOf(envoyGlobal, envoyVte12, envoyVte12Lvte1).forEach { envoy ->
+                    allServices.forEach { service ->
+                        envoy.waitForClusterEndpointHealthy("echo", service.container().ipAddress())
+                    }
                 }
             }
         }
     }
+    @Order(1)
+    @Nested
+    inner class WhenAllServicesAreUp : WhenAllServicesAreUpSetup() {
 
-    @Test
-    fun `requests are routed according to default service tag preference`() {
+        @Test
+        fun `requests are routed according to default service tag preference`() {
+            envoyGlobal.callServiceRepeatedly(service = "echo")
+                .assertAllResponsesOkAndFrom(instance = echoGlobal)
+            envoyVte12.callServiceRepeatedly(service = "echo")
+                .assertAllResponsesOkAndFrom(instance = echoVte12)
+            envoyVte12Lvte1.callServiceRepeatedly(service = "echo")
+                .assertAllResponsesOkAndFrom(instance = echoVte12Lvte1)
+        }
 
-        envoyGlobal.callServiceRepeatedly(service = "echo")
-            .assertAllResponsesOkAndFrom(instance = echoGlobal)
+        @Test
+        fun `x-service-tag-preference from request overrides default one`() {
 
-        envoyVte12.callServiceRepeatedly(service = "echo")
-            .assertAllResponsesOkAndFrom(instance = echoVte12)
+            envoyGlobal.callServiceRepeatedly(service = "echo", serviceTagPreference = "lvte1|vte12|global")
+                .assertAllResponsesOkAndFrom(instance = echoVte12Lvte1)
+            envoyVte12.callServiceRepeatedly(service = "echo", serviceTagPreference = "global")
+                .assertAllResponsesOkAndFrom(instance = echoGlobal)
+            envoyVte12.callServiceRepeatedly(service = "echo", serviceTagPreference = "lvte1|vte12|global")
+                .assertAllResponsesOkAndFrom(instance = echoVte12Lvte1)
+            envoyVte12Lvte1.callServiceRepeatedly(service = "echo", serviceTagPreference = "vte12|global")
+                .assertAllResponsesOkAndFrom(instance = echoVte12)
+        }
 
-        envoyVte12Lvte1.callServiceRepeatedly(service = "echo")
-            .assertAllResponsesOkAndFrom(instance = echoVte12Lvte1)
+        @Test
+        fun `x-service-tag overrides x-service-tag-preference`() {
+
+            envoyGlobal.callServiceRepeatedly(service = "echo", serviceTag = "vte12")
+                .assertAllResponsesOkAndFrom(instance = echoVte12)
+
+            envoyVte12.callServiceRepeatedly(
+                service = "echo",
+                serviceTag = "global",
+                serviceTagPreference = "lvte1|vte12|global"
+            )
+                .assertAllResponsesOkAndFrom(instance = echoGlobal)
+        }
     }
 
-    @Test
-    fun `x-service-tag-preference from request overrides default one`() {
-
-        envoyGlobal.callServiceRepeatedly(service = "echo", serviceTagPreference = "lvte1|vte12|global")
-            .assertAllResponsesOkAndFrom(instance = echoVte12Lvte1)
-
-        envoyVte12.callServiceRepeatedly(service = "echo", serviceTagPreference = "global")
-            .assertAllResponsesOkAndFrom(instance = echoGlobal)
-
-        envoyVte12.callServiceRepeatedly(service = "echo", serviceTagPreference = "lvte1|vte12|global")
-            .assertAllResponsesOkAndFrom(instance = echoVte12Lvte1)
-
-        envoyVte12Lvte1.callServiceRepeatedly(service = "echo", serviceTagPreference = "vte12|global")
-            .assertAllResponsesOkAndFrom(instance = echoVte12)
+    open class ThenLvteInstanceIsDownSetup {
+        companion object {
+            @JvmStatic
+            @BeforeAll
+            fun deregisterLvte() {
+                deregisterInstance(echoVte12Lvte1)
+            }
+        }
     }
+    @Order(2)
+    @Nested
+    inner class ThenLvteInstanceIsDown : ThenLvteInstanceIsDownSetup() {
 
-    @Test
-    fun `x-service-tag overrides x-service-tag-preference`() {
-
-        envoyGlobal.callServiceRepeatedly(service = "echo", serviceTag = "vte12")
-            .assertAllResponsesOkAndFrom(instance = echoVte12)
-
-        envoyVte12.callServiceRepeatedly(
-            service = "echo",
-            serviceTag = "global",
-            serviceTagPreference = "lvte1|vte12|global"
-        )
-            .assertAllResponsesOkAndFrom(instance = echoGlobal)
+        @Test
+        fun `LVTE request falls back to base VTE instance`() {
+            envoyGlobal.callServiceRepeatedly(service = "echo")
+                .assertAllResponsesOkAndFrom(instance = echoGlobal)
+            envoyVte12.callServiceRepeatedly(service = "echo")
+                .assertAllResponsesOkAndFrom(instance = echoVte12)
+            envoyVte12Lvte1.callServiceRepeatedly(service = "echo")
+                .assertAllResponsesOkAndFrom(instance = echoVte12)
+        }
     }
 
     /**
@@ -137,35 +172,47 @@ class ServiceTagPreferenceTest {
      *
      */
 
-    private val repeat = 10
-
-    private fun EnvoyExtension.callServiceRepeatedly(
-        service: String,
-        serviceTagPreference: String? = null,
-        serviceTag: String? = null
-    ): CallStats =
-        this.egressOperations.callServiceRepeatedly(
-            service = service, stats = CallStats(allServices), minRepeat = repeat, maxRepeat = repeat,
-            headers = buildMap {
-                if (serviceTagPreference != null) {
-                    put("x-service-tag-preference", serviceTagPreference)
+    private object Extensions {
+        private const val REPEAT = 10
+        fun EnvoyExtension.callServiceRepeatedly(
+            service: String,
+            serviceTagPreference: String? = null,
+            serviceTag: String? = null
+        ): CallStats =
+            this.egressOperations.callServiceRepeatedly(
+                service = service, stats = CallStats(allServices), minRepeat = REPEAT, maxRepeat = REPEAT,
+                headers = buildMap {
+                    if (serviceTagPreference != null) {
+                        put("x-service-tag-preference", serviceTagPreference)
+                    }
+                    if (serviceTag != null) {
+                        put("x-service-tag", serviceTag)
+                    }
                 }
-                if (serviceTag != null) {
-                    put("x-service-tag", serviceTag)
-                }
-            }
-        )
+            )
 
-    private fun CallStats.assertAllResponsesOkAndFrom(instance: UpstreamService) {
-        assertThat(failedHits).isEqualTo(0)
-        assertThat(hits(instance))
-            .describedAs {
-                "hits: {" +
-                    "global: ${hits(echoGlobal)}, " +
-                    "vte12: ${hits(echoVte12)}, " +
-                    "lvte1: ${hits(echoVte12Lvte1)}, " +
-                    "vte33: ${hits(echoVte33)}}"
-            }
-            .isEqualTo(totalHits).isEqualTo(repeat)
+        fun CallStats.assertAllResponsesOkAndFrom(instance: UpstreamService) {
+            assertThat(failedHits).isEqualTo(0)
+            assertThat(hits(instance))
+                .describedAs {
+                    "hits: {" +
+                        "global: ${hits(echoGlobal)}, " +
+                        "vte12: ${hits(echoVte12)}, " +
+                        "lvte1: ${hits(echoVte12Lvte1)}, " +
+                        "vte33: ${hits(echoVte33)}}"
+                }
+                .isEqualTo(totalHits).isEqualTo(REPEAT)
+        }
+
+        fun registerInstance(name: String, tags: List<String>, extension: ServiceExtension<*>) {
+            val id = consul.server.operations.registerService(name = name, tags = tags, extension = extension)
+            consulRegisteredServicesIds[extension] = id
+        }
+        fun deregisterInstance(extension: ServiceExtension<*>) {
+            val removed = consulRegisteredServicesIds.remove(extension)
+            requireNotNull(removed) { "Extension was already unregistered!" }
+            consul.server.operations.deregisterService(removed)
+        }
+        val consulRegisteredServicesIds: MutableMap<ServiceExtension<*>, String> = mutableMapOf()
     }
 }
